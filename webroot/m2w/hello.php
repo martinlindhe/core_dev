535eee94bf526f328e6dcb7c434c347049c5c4d4
Martin Lindhe <martin@ubique.se>
2007-09-11 11:55:33 +0000

    m2w test-applikation

---
+++ webroot/m2w/hello.php
@@ -0,0 +1,42 @@
+<?
+	echo file_get_contents('vxml_head.xml');
+?>
+
+	<form id="jingle">
+		<block>
+			<!-- this audio will loop over all the following menus -->
+		  <pse_audio src="jingle.wav" repeat="LOOP"/>
+		  <goto next="#mnuMain"/>
+		 </block>
+	</form>
+
+  <!-- main menu -->
+	<menu id="mnuMain">
+	  <pse_video src="media://m2w/mnuMain1" repeat="LOOP"/>
+		
+		<choice dtmf="1" next="#mnuRecord"></choice>
+		<choice dtmf="2" next="#mnuPlayback"></choice>
+		<choice dtmf="0" next="#quit"></choice>
+	</menu>
+
+	<menu id="mnuRecord">
+	  <pse_video src="media://m2w/mnuRecord1" repeat="LOOP"/>
+		
+		<choice dtmf="0" next="#mnuMain"></choice>
+	</menu>
+
+	<menu id="mnuPlayback">
+	  <pse_video src="media://m2w/mnuPlayback1" repeat="LOOP"/>
+		
+		<choice dtmf="0" next="#mnuMain"></choice>
+	</menu>
+
+  <!-- Quit block -->
+	<form id="quit">
+		<block>
+		  <exit/>
+		</block>
+	</form>
+<?
+	echo file_get_contents('vxml_foot.xml');
+?>
