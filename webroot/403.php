1ce3f2bb4c77e1dbdb41b68147c3ec59c73a0bdf
Martin Lindhe <martin@ubique.se>
2007-09-12 17:49:56 +0000

    403.php added, 404.php xhtml cleanup

---
+++ webroot/403.php
@@ -0,0 +1,14 @@
+<?
+	//echo '<pre>'; print_r($_SERVER);
+	echo '<h1>Access blocked</h1>';
+	echo $_SERVER['REQUEST_URI'].' is blocked for '.$_SERVER['REMOTE_ADDR'].'. you are logged<br/><br/>';
+
+	if ($_SERVER['REQUEST_URI'] != '/') {
+		echo '&middot; <a href="/">Go to site index</a><br/>';
+	}
+
+	if (!empty($_SERVER['HTTP_REFERER'])) {
+		echo '&middot; <a href="'.$_SERVER['HTTP_REFERER'].'">Go back to referring page</a><br/>';
+	}
+	die;
+?>
