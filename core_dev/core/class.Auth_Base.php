0b4f2d7b93fcddbae60b60e154399b635c384373
Martin Lindhe <martin@ubique.se>
2008-02-05 12:55:20 +0000

    added phpmailer 2.1.0 beta 2

--- core_dev/core/class.Auth_Base.php
+++ core_dev/core/class.Auth_Base.php
@@ -4,7 +4,7 @@
  *
  * Skeleton for authentication modules
  *
- * \todo a libapache2-mod-auth-openid module
+ * \todo libapache2-mod-auth-openid module
  *
  * \author Martin Lindhe, 2007-2008 <martin@startwars.org>
  */
@@ -17,6 +17,7 @@ abstract class Auth_Base
 	public $allow_registration = true;		///< set to false to disallow the possibility to register new users. will be disabled if login is disabled
 	public $reserved_usercheck = true;		///< check if username is listed as reserved username, requires tblStopwords
 	public $userdata = true; 							///< shall we use tblUserdata for required userdata fields?
+	public $mail_activate = false;				///< does account registration require email activation?
 
 	function __construct(array $auth_conf = array())
 	{
@@ -25,19 +26,20 @@ abstract class Auth_Base
 		if (isset($auth_conf['sha1_key'])) $this->sha1_key = $auth_conf['sha1_key'];
 		if (isset($auth_conf['allow_login'])) $this->allow_login = $auth_conf['allow_login'];
 		if (isset($auth_conf['allow_registration'])) $this->allow_registration = $auth_conf['allow_registration'];
-		if (isset($auth_conf['userdata'])) $this->userdata = $auth_conf['userdata'];
 		if (isset($auth_conf['reserved_usercheck'])) $this->reserved_usercheck = $auth_conf['reserved_usercheck'];
+		if (isset($auth_conf['userdata'])) $this->userdata = $auth_conf['userdata'];
+		if (isset($auth_conf['mail_activate'])) $this->mail_activate = $auth_conf['mail_activate'];
 
 		$this->handleAuthEvents();
 	}
 
 	abstract function registerUser($username, $password1, $password2, $userMode = 0);
 
+	//FIXME abstract function unregisterUser()
+
 	abstract function login($username, $password);
 
 	abstract function logout();
-
-	//FIXME abstract function unregisterUser()
 
 	abstract function showLoginForm();
 
@@ -65,13 +67,23 @@ abstract class Auth_Base
 		if ($this->allow_registration && !$session->id && isset($_POST['register_usr']) && isset($_POST['register_pwd']) && isset($_POST['register_pwd2'])) {
 			$check = $this->registerUser($_POST['register_usr'], $_POST['register_pwd'], $_POST['register_pwd2']);
 			if (is_numeric($check)) {
-				//FIXME: implement activation
-				$this->login($_POST['register_usr'], $_POST['register_pwd']);
+				if ($this->mail_activate) {
+					$this->sendActivationMail($check);
+				} else {
+					$this->login($_POST['register_usr'], $_POST['register_pwd']);
+				}
 			} else {
 				$session->error = 'Registration failed, '.$check;
 			}
 		}
+	}
 
+	/**
+	 * Sends a account activation mail to specified user
+	 */
+	function sendActivationMail($_id)
+	{
+		if (!is_numeric($_id)) return false;
 	}
 
 }
