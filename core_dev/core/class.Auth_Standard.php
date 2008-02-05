4693fa2237fddd4e5476c3e21990328b2f624201
Martin Lindhe <martin@ubique.se>
2008-02-05 11:32:13 +0000

    - core/class.Auth_Standard.php: shows last entered username on failed
    		registration attempt

--- core_dev/core/class.Auth_Standard.php
+++ core_dev/core/class.Auth_Standard.php
@@ -179,7 +179,7 @@ class Auth_Standard extends Auth_Base
 				echo '<table cellpadding="2">';
 				echo '<tr>'.
 								'<td>Username:</td>'.
-								'<td><input name="register_usr" type="text"'.(!empty($_POST['login_usr'])?' value="'.$_POST['login_usr'].'"':'').'/> '.
+								'<td><input name="register_usr" type="text"'.(!empty($_POST['register_usr'])?' value="'.$_POST['register_usr'].'"':'').'/> '.
 									'<img src="'.$config['core_web_root'].'gfx/icon_user.png" alt="Username"/>'.
 								'</td>'.
 							'</tr>';
