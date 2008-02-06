668678eb2637e208a5ec9768032531efa845ea85
Martin Lindhe <martin@ubique.se>
2008-02-06 15:18:45 +0000

    - admin/admin_statistics.php & friends: improved stats display

--- core_dev/admin/admin_statistics.php
+++ core_dev/admin/admin_statistics.php
@@ -10,16 +10,35 @@
 
 	echo '<h1>Statistics</h1>';
 
-	/*for ($m = 1; $m <= date('n'); $m++) {
-		echo 'Month '.$m.' 2007:<br/>';
-		//generateStatsMonth(2007, $m);
-		showStatsMonth(2007, $m);
-	}*/
-	
-	$yr = date('Y');
-	$mn = date('n');
 
-	showStatsMonth($yr, $mn);
+	if (!empty($_GET['y']) && !empty($_GET['m'])) {
+		showStatsMonth($_GET['y'], $_GET['m']);
+
+		echo '<br/><br/><a href="'.$_SERVER['PHP_SELF'].getProjectPath(0).'">Return to overview</a>';
+
+	} else {
+		//list overview of all months from oldest entry in db until today
+
+		$start_date = strtotime(getOldestLoginTime());
+		$end_date = time();
+
+		$start_year = date('Y', $start_date);
+		$end_year = date('Y', $end_date);
+
+		$start_month = date('n', $start_date);
+		$end_month = date('n', $end_date);
+
+		for ($yr = $start_year; $yr <= $end_year; $yr++) {
+			echo '<h2>'.$yr.'</h2>';
+
+			for ($m = ($yr == $start_year ? $start_month : 1); $m <= ($yr == $end_year ? $end_month : 12); $m++) {
+				$curr = mktime(0, 0, 0, $m, 1, $yr);
+
+				echo '<a href="'.$_SERVER['PHP_SELF'].'?m='.$m.'&y='.$yr.getProjectPath().'">'.date('F', $curr).', '.$yr.'</a><br/>';
+			}
+			echo '<br/>';
+		}
+	}
 
 	require($project.'design_foot.php');
 ?>
