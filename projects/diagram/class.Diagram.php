42cbfe1f4c2f223956141915eaccbe29c2675bc7
Martin Lindhe <martin@ubique.se>
2008-04-01 18:45:30 +0000

    bara pill

--- projects/diagram/class.Diagram.php
+++ projects/diagram/class.Diagram.php
@@ -12,9 +12,10 @@ class Diagram
 	private $height = 400;	///< Height of output image
 	private $bg_r = 150, $bg_g = 150, $bg_b = 150;	///< Background color
 	private $txt_r = 233, $txt_g = 220, $txt_b = 110;	///< Text color
-	private $font = 'arial.ttf';	///< fixme make changeable
+	private $line_r = 0, $line_g = 0, $line_b = 0;	///< Line color
+	private $font = './arial.ttf';	///< fixme make changeable
 	private $ttf_size = 14;			///< fixme make changeable
-	private $txt_col;
+	private $bg_col, $txt_col, $line_col;
 
 	private $vline_min = 1, $vline_max = 10, $vline_step = 1, $vline_text = '';	///< Settings for vertical line of diagram
 	private $hline_min = 1, $hline_max = 20, $hline_step = 0.5, $hline_text = '';	///< Settings for horizontal line of diagram
@@ -82,6 +83,13 @@ class Diagram
 		$this->txt_b = $b;
 	}
 
+	function LineCol($r, $g, $b)
+	{
+		$this->line_r = $r;
+		$this->line_g = $g;
+		$this->line_b = $b;
+	}
+
 	/**
 	 * Outputs the resulting image to a PNG file
 	 */
@@ -111,8 +119,9 @@ class Diagram
 	{
 		$this->im = imagecreate($this->width, $this->height);
 
-		$bg_col = imagecolorallocate($this->im, $this->bg_r, $this->bg_g, $this->bg_b);
+		$this->bg_col = imagecolorallocate($this->im, $this->bg_r, $this->bg_g, $this->bg_b);
 		$this->txt_col = imagecolorallocate($this->im, $this->txt_r, $this->txt_g, $this->txt_b);
+		$this->line_col = imagecolorallocate($this->im, $this->line_r, $this->line_g, $this->line_b);
 
 		$fh = $this->ttfHeight($this->vline_text);
 		$v_len = $this->ttfWidth($this->vline_text);
@@ -128,11 +137,38 @@ class Diagram
 		$hy = $this->height-($fh/2);
 		$this->ttfHText($hx, $hy, $this->hline_text);
 
+
+
+
 		//Draw vertical scale - TODO
-		for ($i = $this->vline_min; $i < $this->vline_max; $i <+ $this->vline_step) {
+		$v_steps = 0;
+		for ($i = $this->vline_min; $i <= $this->vline_max; $i += $this->vline_step) {
+			$v_steps++;
 		}
 
+		//Draw vertical line
+		imageline($this->im, $vx + $fh, 40, $vx + $fh, $this->height-40, $this->line_col);
+
+
+
+
+
 		//Draw horizontal scale1 - TODO
+		$h_steps = 0;
+		for ($i = $this->hline_min; $i <= $this->hline_max; $i += $this->hline_step) {
+			$h_steps++;
+		}
+
+		$h_width = $this->width - 40*2;	//40 px padding on each side
+		$h_multiplier = $h_width / $h_steps;
+
+		for ($i = 0; $i <= $h_steps; $i++) {
+			imagesetpixel($this->im, 40+($i*$h_multiplier), $this->height-50,$this->txt_col);
+		}
+
+		//Draw horizontal line
+		imageline($this->im, 40, $this->height - 40, $this->width-40, $this->height-40, $this->line_col);
+
 
 		//Draw diagram content - TODO
 	}
--- projects/diagram/test.php
+++ projects/diagram/test.php
@@ -5,14 +5,15 @@ require('class.Diagram.php');
 
 $d = new Diagram();
 
-$d->VLine(0, 4, 0.5);	//min,max,step
+$d->VLine(0, 10000, 1000);	//min,max,step.	FIXME hmm... detta kanske ska räknas ut av datan...
 $d->VText("Changes per release");
 
-$d->HLine(11, 24, 1);
+$d->HLine(11, 24, 1);		//FIXME kanske ska räknas ut...
 $d->HText("Days of development");
 
 $d->BGCol(200,200,155);
 $d->TextCol(0, 0, 0);
+$d->LineCol(255,255,255);
 
 $d->Size(500, 300); //pixels
 $d->Display();
