274323c9d31e15680345e1183072a66af418376a
Martin Lindhe <martin@ubique.se>
2008-04-15 14:52:47 +0000

    dont log visits for guests

--- core_dev/core/functions_visits.php
+++ core_dev/core/functions_visits.php
@@ -11,7 +11,7 @@
 	function logVisit($_type, $_owner)
 	{
 		global $db, $session;
-		if (!is_numeric($_type) || !is_numeric($_owner)) return false;
+		if (!$session->id || !is_numeric($_type) || !is_numeric($_owner)) return false;
 
 		//only log the latest entry for each visitor
 		$q = 'DELETE FROM tblVisits WHERE type='.$_type.' AND ownerId='.$_owner.' AND creatorId='.$session->id;
