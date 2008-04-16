c721d50e88fb9792483f0bfa0db2f71f0bb4a96b
Martin Lindhe <martin@ubique.se>
2008-04-16 10:17:22 +0000

    verifyActivationOnly() new function improved doxygen comments

--- core_dev/core/functions_activate.php
+++ core_dev/core/functions_activate.php
@@ -7,6 +7,7 @@
  * \todo cleanup script that deletes > 30 day old entries from tblActivation
  * \todo cleanup script that deletes un-activated users entirely
  * \todo finish api/human_test.php implementation
+ * \todo rename to atom_activation.php
  *
  * \author Martin Lindhe, 2007-2008 <martin@startwars.org>
  */
@@ -17,11 +18,18 @@
 	define('ACTIVATE_CHANGE_PWD',	4);		//used to allow the user to set a new password from a email link when he forgot password
 	define('ACTIVATE_ACCOUNT',		5);		//used to activate a pre-generated account
 
-	$config['activate']['expire_time_captcha']		= 60*5;			// 5 minutes
-	$config['activate']['expire_time_email']		= (12*60*60); 	// 12 hours
-	$config['activate']['expire_time_sms']			= (12*60*60);	// 12 hours
-	$config['activate']['expire_time_change_pwd']	= (6*60*60); 	// 6 hours
+	$config['activate']['expire_time_captcha']		= 60*5;			///< 5 minutes
+	$config['activate']['expire_time_email']		= (12*60*60); 	///< 12 hours
+	$config['activate']['expire_time_sms']			= (12*60*60);	///< 12 hours
+	$config['activate']['expire_time_change_pwd']	= (6*60*60); 	///< 6 hours
 
+	/**
+	 * Returns an unused numeric activation code
+	 *
+	 * \param $lo lower limit of code
+	 * \param $hi upper limit of code
+	 * \return unused numeric activation code
+	 */
 	function generateActivationCode($lo, $hi)
 	{
 		global $db;
@@ -35,7 +43,10 @@
 	}
 
 	/**
+	 * Verify if activation code is valid
 	 *
+	 * \param $_type type
+	 * \param $_rnd activation code
 	 * \param $_answer is string for CAPTCHA's, userId for email/sms activation
 	 */
 	function verifyActivation($_type, $_rnd, $_answer = '')
@@ -46,8 +57,8 @@
 		switch ($_type)
 		{
 			case ACTIVATE_CAPTCHA:		$expired = $config['activate']['expire_time_captcha']; break;
-			case ACTIVATE_EMAIL:			$expired = $config['activate']['expire_time_email']; break;
-			case ACTIVATE_SMS:				$expired = $config['activate']['expire_time_sms']; break;
+			case ACTIVATE_EMAIL:		$expired = $config['activate']['expire_time_email']; break;
+			case ACTIVATE_SMS:			$expired = $config['activate']['expire_time_sms']; break;
 			case ACTIVATE_CHANGE_PWD:	$expired = $config['activate']['expire_time_change_pwd']; break;
 		}
 
@@ -71,19 +82,35 @@
 	}
 
 	/**
-	 * xxx
+	 * Only checks if the activation code is valid, doesn't require association of activation code & user id
 	 *
 	 * \param $_type type
-	 * \param $_rnd xx
+	 * \param $_rnd activation code
+	 */
+	function verifyActivationOnly($_type, $_rnd)
+	{
+		global $db, $config;
+		if (!is_numeric($_type)) return false;
+
+		//TODO: check expiration time!
+		$q = 'SELECT COUNT(entryId) FROM tblActivation WHERE type='.$_type.' AND rnd="'.$db->escape($_rnd).'"';
+		return $db->getOneItem($q);
+	}
+
+	/**
+	 * Creates a new activation code
+	 *
+	 * \param $_type type
+	 * \param $_code activation code
 	 * \param $_answer is correct answer to captcha-implementation, or userId for email/sms activation
 	 * \return activationId
 	 */
-	function createActivation($_type, $_rnd, $_answer = '')
+	function createActivation($_type, $_code, $_answer = '')
 	{
 		global $db;
 		if (!is_numeric($_type)) return false;
 
-		$q = 'INSERT INTO tblActivation SET type='.$_type.',rnd="'.$db->escape($_rnd).'",timeCreated=NOW()';
+		$q = 'INSERT INTO tblActivation SET type='.$_type.',rnd="'.$db->escape($_code).'",timeCreated=NOW()';
 		switch ($_type) {
 			case ACTIVATE_CAPTCHA:
 				$q .= ', answer="'.$db->escape($_answer).'"';
@@ -121,14 +148,14 @@
 	 * Removes a single activation code. Call this when activation process has succeeded
 	 *
 	 * \param $_type type
-	 * \param $_rnd xx
+	 * \param $_rnd activation code
 	 */
-	function removeActivation($_type, $_rnd)
+	function removeActivation($_type, $_code)
 	{
 		global $db;
-		if (!is_numeric($_type) || !is_numeric($_rnd)) return false;
+		if (!is_numeric($_type) || !is_numeric($_code)) return false;
 
-		$q = 'DELETE FROM tblActivation WHERE type='.$_type.' AND rnd="'.$db->escape($_rnd).'"';
+		$q = 'DELETE FROM tblActivation WHERE type='.$_type.' AND rnd="'.$db->escape($_code).'"';
 		$db->delete($q);
 	}
 ?>
