10716d2083a37865e49139ea7be96b8b73815b03
Martin Lindhe <martin@ubique.se>
2008-05-14 14:07:40 +0000

    ny lönings-kalender

--- projects/calendar/loning.php
+++ projects/calendar/loning.php
@@ -1,1 +1,15 @@
-x
+<?php
+
+require_once('functions_icalendar.php');
+
+header('Content-Type: text/plain; charset="UTF-8"');
+
+iCalBegin('VCALENDAR', 'Svenska Helgdagar');
+for ($i = date('Y')-1; $i <= date('Y')+1; $i++)
+{
+	$cal = calPaydaysMonthly($i, 25);
+	iCalEvents($cal, 'Europe/Stockholm');	//$cal->renderiCal();
+}
+iCalEnd('VCALENDAR');
+
+?>
