8341fb6a63307a6a860003fdc7df6728865c0cd1
Martin Lindhe <martin@ubique.se>
2008-05-14 14:16:30 +0000

    indexsida

---
+++ projects/calendar/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+	<title>Svenska kalendrar i iCalendar-format</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+</head>
+
+<body>
+	Svenska kalendrar i iCalendar-format för användning i Apple iCal eller Mozilla Thunderbird (med Lightning)<br/>
+	<br/>
+	<a href="helgdagar.php">Svenska Helgdagar</a><br/>
+	<br/>
+	<a href="loning.php">Månadslön (den vardag som infaller på eller direkt innan den 25:e)</a><br/>
+</body>
+</html>
