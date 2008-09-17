2b6628591403ebbf9c887de2e0ddc1771f5ccb74
Linus Wiklund <linus@unicorn.se>
2008-09-17 11:51:12 +0000

    Fix the other script in the cron dir

--- core_dev/cron/cleanup_activations.php
+++ core_dev/cron/cleanup_activations.php
@@ -5,7 +5,7 @@
 
 define('MAX_LIFETIME', 30);	//delete entries older than 30 days
 
-require_once('/home/ml/dev/pc/config.php');	//FIXME: no hardcoded path
+require_once('find_config.php');
 
 $q = 'DELETE FROM tblActivation WHERE timeCreated <= DATE_SUB(NOW(),INTERVAL '.MAX_LIFETIME.' DAY)';
 $db->delete($q);
