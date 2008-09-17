2d3c1cc91579d3b3c0281c80ee661aedbce36f41
Linus Wiklund <linus@unicorn.se>
2008-09-17 14:57:13 +0000

    oh.. yes. we need to be able to find the statistical functions too! how strange!

--- core_dev/cron/find_config.php
+++ core_dev/cron/find_config.php
@@ -8,6 +8,7 @@ $project = '../../';
 require_once($project.'config.php');
 
 set_include_path($config['core']['fs_root'].'core/');
+require_once('functions_statistics.php');
 require_once('functions_forum.php');
 restore_include_path();
 ?>
