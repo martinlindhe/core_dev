2fb8123ac80f9837f8bd87a163e382694a63adde
Martin Lindhe <martin@ubique.se>
2008-09-24 10:24:32 +0000

    revert

--- projects/process/show_queue.php
+++ projects/process/show_queue.php
@@ -66,7 +66,6 @@ if (!empty($list)) {
 				echo '<a href="show_file_status.php?id='.$row['referId'].'">Show file status</a><br/>';
 			}
 
-			$files->updateFile($row['referId']);
 			$file = $files->getFileInfo($row['referId']);
 			if ($file) {
 				echo '<h3>Source file:</h3>';
