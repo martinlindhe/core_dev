0488796a024382372a7201f2f162eb48eb85f3ad
Martin Lindhe <martin@ubique.se>
2008-11-19 18:59:54 +0000

    beginning of attachment support, still not functional

--- core_dev/core/class.Sendmail.php
+++ core_dev/core/class.Sendmail.php
@@ -8,6 +8,8 @@
 /**
  * TODO: ability to add file attachments to the mail
  * TODO: ability to embed graphics in the mail (html)
+ * TODO: ability to add "Reply-To:" header
+ * TODO: use regexp to catch invalid mail address input
  */
 
 require_once('output_smtp.php');
@@ -19,6 +21,7 @@ class Sendmail
 	var $from_adr;
 	var $to_adr = array(), $cc_adr = array(), $bcc_adr = array();
 	var $html = false;
+	var $attachments = array();
 
 	function __construct($server = '', $username = '', $password = '', $port = 25)
 	{
@@ -48,6 +51,16 @@ class Sendmail
 		$this->bcc_adr[] = $s;
 	}
 
+	function attach($file)
+	{
+		if (!file_exists($file)) {
+			echo "Error: File ".$file." not found\n";
+			return false;
+		}
+		$this->attachments[] = $file;
+		return true;
+	}
+
 	/**
 	 * Sends a email
 	 */
@@ -70,16 +83,32 @@ class Sendmail
 			$header .= "Bcc: ".$bcc."\r\n";
 		}
 
+		$attachment_data = '';
+		foreach ($this->attachments as $a) {
+			$data = file_get_contents($a);
+			$chunk = chunk_split(base64_encode($data));
+			$attachment_data .=
+			"--MIME_BOUNDARY\r\n".
+			"Content-Type: image/png; name=\"".basename($a)."\"\r\n".	//XXX: get mimetype
+			"Content-disposition: attachment\r\n".
+			"Content-Transfer-Encoding: base64\r\n\r\n".
+			$chunk."\r\n\r\n";
+		}
+
 		$header .=
 		"From: ".$this->from_adr."\r\n".
 		"Subject: ".$subject."\r\n".
 		"Date: ".date('r')."\r\n".
 		"X-Mailer: core_dev\r\n".	//XXX version string
-		"MIME-Version: 1.0\r\n".
-		"Content-Type: ".($this->html ? 'text/html' : 'text/plain')."; charset=utf-8\r\n".
-		"\r\n";
+		"MIME-Version: 1.0\r\n";
+		if (count($this->attachments)) {
+			$header .= "Content-Type: multipart/mixed; boundary=\"MIME_BOUNDARY\"";
+		} else {
+			$header .= "Content-Type: ".($this->html ? 'text/html' : 'text/plain')."; charset=utf-8\r\n";
+		}
+		$header .= "\r\n";
 
-		return $this->smtp->_DATA($header.$msg);
+		return $this->smtp->_DATA($header.$msg.$attachment_data);
 	}
 
 }
