930ec06ce38065771ecdd3586d3d0755ed8d8a90
Martin Lindhe <martin@ubique.se>
2008-12-02 09:40:06 +0000

    intendation

--- core_dev/core/atom_logging.php
+++ core_dev/core/atom_logging.php
@@ -14,12 +14,11 @@ define('LOGLEVEL_ALL',		5);
 
 function logEntry($str, $entryLevel = LOGLEVEL_NOTICE, $userId = 0, $IP = 0)
 {
-		global $db;
-		if (!is_numeric($entryLevel)) return false;
+	global $db;
+	if (!is_numeric($entryLevel)) return false;
 
-		$q = 'INSERT INTO tblLogs SET entryText="'.$db->escape($str).'",entryLevel='.$entryLevel.',timeCreated=NOW(),userId='.$userId.',userIP='.$IP;
-		return $db->insert($q);
-
+	$q = 'INSERT INTO tblLogs SET entryText="'.$db->escape($str).'",entryLevel='.$entryLevel.',timeCreated=NOW(),userId='.$userId.',userIP='.$IP;
+	return $db->insert($q);
 }
 
 ?>
