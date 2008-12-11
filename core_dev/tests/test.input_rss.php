633f0b57b4daf174bbbf57300237158d09f0e698
Martin Lindhe <martin@ubique.se>
2008-12-11 14:15:17 +0000

    improved input_rss example

--- core_dev/tests/test.input_rss.php
+++ core_dev/tests/test.input_rss.php
@@ -2,9 +2,21 @@
 
 require('/var/www/core_dev/core/input_rss.php');
 
+/**
+ * Example in how to make a rss fetcher that only notifies on new content in the feed
+ */
 function rssCallback($data)
 {
+	$sha1 = sha1($data['link']);	//may want to use other factors to determine the uniqeness
+
+	$cache_path = '/tmp/rsstest/';
+	if (!file_exists($cache_path)) mkdir($cache_path);
+	if (file_exists($cache_path.$sha1)) return;
+
+	//only displayed if item is new
 	print_r($data);
+
+	touch($cache_path.$sha1);
 }
 
 $url = 'http://tvrss.net/search/index.php?show_name=24&show_name_exact=true&mode=rss';
