f64ac5eb19c63e6c24a418fe067d9d56d1298def
Martin Lindhe <martin@ubique.se>
2008-12-12 13:15:22 +0000

    added alpha

--- core_dev/core/output_svg.php
+++ core_dev/core/output_svg.php
@@ -16,7 +16,7 @@
  * @author Martin Lindhe, 2008 <martin@startwars.org>
  */
 
-//TODO: define transpacency on polygons
+//FIXME: opacity is not quite correct
 
 class svg
 {
@@ -63,13 +63,28 @@ class svg
 			'<rect x="0" y="0" width="'.$this->width.'" height="'.$this->height.'" fill="'.sprintf('%06x', $this->bgcolor).'"/>';
 		}
 
-//fill-opacity:0.2; stroke-opacity:0.8
 		foreach ($this->polygons as $poly) {
+			$fill_a = ($poly['color'] >> 24) & 0xFF;
+			$fill_a = round($fill_a/127, 2);		//XXX loss of precision
+			$poly['color'] = $poly['color'] & 0xFFFFFF;
+
+			if (!empty($poly['border'])) {
+				$stroke_a = ($poly['border'] >> 24) & 0xFF;
+				$stroke_a = round($stroke_a/127, 2);
+				$poly['border'] = $poly['border'] & 0xFFFFFF;
+			}
+
 			$res .=
 			'<polygon'.
 				' fill="#'.sprintf('%06x', $poly['color']).'"'.
-				(!empty($poly['border']) ? ' stroke-width="1" stroke="#'.sprintf('%06x', $poly['border']).'"' : '').
-				' points="';
+				($fill_a < 1 ? ' fill-opacity="'.$fill_a.'"' : '');
+				if (!empty($poly['border'])) {
+					$res .=
+					' stroke-width="1" stroke="#'.sprintf('%06x', $poly['border']).'"'.
+					($stroke_a < 1 ? ' stroke-opacity="'.$stroke_a.'"': '');
+				}
+
+			$res .= ' points="';
 			for ($i=0; $i<count($poly['coords']); $i+=2) {
 				$res .= $poly['coords'][$i].','.$poly['coords'][$i+1];
 				if ($i < count($poly['coords'])-2) $res .= ',';
