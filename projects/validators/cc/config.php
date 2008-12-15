b465a0ea73cd95471640e22c1a10ad01c23fee82
Martin Lindhe <martin@ubique.se>
2008-12-15 18:04:56 +0000

    function files was renamed

--- projects/validators/cc/config.php
+++ projects/validators/cc/config.php
@@ -9,8 +9,8 @@ $config['app']['web_root'] = '/core_dev/validators/cc/';
 $config['default_title'] = 'credit card validator';
 
 set_include_path($config['core']['fs_root'].'core/');
-require_once('functions_validate_cc.php');
 require_once('functions_general.php');
+require_once('validate_cc.php');
 restore_include_path();
 
 $config['debug'] = true;
--- projects/validators/ssn/config.php
+++ projects/validators/ssn/config.php
@@ -5,8 +5,8 @@ error_reporting(E_ALL);
 $config['core']['fs_root'] = '../../';
 
 set_include_path($config['core']['fs_root'].'core/');
-require_once('functions_validate_ssn.php');
 require_once('functions_general.php');
+require_once('validate_ssn.php');
 require_once('functions_textformat.php');
 restore_include_path();
 
