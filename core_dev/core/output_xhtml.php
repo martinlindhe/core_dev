2858ce18c8062b3ff70ebeb0fd405967ed315ebe
Martin Lindhe <martin@ubique.se>
2009-03-04 13:33:37 +0000

    object-oriented xhtml header generator

--- core_dev/core/output_xhtml.php
+++ core_dev/core/output_xhtml.php
@@ -5,7 +5,7 @@
  * Helper functions for rapid XHTML generation
  *
  * @disclaimer This file is a required component of core_dev
- * @author Martin Lindhe, 2007-2008 <martin@startwars.org>
+ * @author Martin Lindhe, 2007-2009 <martin@startwars.org>
  */
 
 require_once('locale.php');
---
+++ core_dev/core/xhtml_header.php
@@ -0,0 +1,165 @@
+<?php
+/**
+ * $Id$
+ *
+ * Class to generate XHTML compilant header
+ *
+ * @author Martin Lindhe, 2009 <martin@startwars.org>
+ */
+
+//TODO: deprecate output_xhtml.php createXHTMLHeader()
+
+require_once('output_xhtml.php');
+
+class xhtml_header
+{
+	var $title   = '';
+	var $favicon = '';
+
+	var $feeds  = array();
+	var $search = array();
+	var $js     = array();
+	var $css    = array();
+	var $onLoad = array();
+
+	/**
+	 * <title> of current page
+	 */
+	function title($t)
+	{
+		$this->title = $t;
+	}
+
+	/**
+	 * Sets URI of favicon to use
+	 */
+	function favicon($uri)
+	{
+		$this->favicon = $uri;
+	}
+
+	/**
+	 * Adds a RSS feed to expose for current page
+	 */
+	function addFeed($uri)
+	{
+		$this->feeds[] = $uri;
+	}
+
+	/**
+	 * Adds a OpenSearch search engine to expose for current page
+	 */
+	function addSearch($uri)
+	{
+		$this->search[] = $uri;
+	}
+
+	/**
+	 * Adds a JS file that needs to be included for current page
+	 */
+	function addJS($uri)
+	{
+		$this->js[] = $uri;
+	}
+
+	/**
+	 * Adds a CSS file that needs to be included for current page
+	 */
+	function addCSS($uri)
+	{
+		$this->css[] = $uri;
+	}
+
+	/**
+	 * Javascript functions/code to execute on page load
+	 */
+	function addOnLoad($js)
+	{
+		$this->onLoad[] = $js;
+	}
+
+	/**
+	 * Creates a complete XHTML header, showing rss feeds if available, etc
+	 */
+	function render()
+	{
+		global $config, $h, $files;
+
+		echo '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">';
+		echo '<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">';
+		echo '<head>';
+		echo '<title>'.$this->title.'</title>';
+		echo '<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>';
+
+		if (!empty($config['core']['web_root'])) {
+			echo '<link rel="stylesheet" type="text/css" href="'.$config['core']['web_root'].'css/core.css"/>';
+		}
+
+		foreach ($this->css as $css) {
+			echo '<link rel="stylesheet" type="text/css" href="'.$css.'"/>';
+		}
+
+		foreach ($this->feeds as $feed) {
+			//XXX: clean up feed URI's etc, make it more general
+			if (!empty($feed['category']) && is_numeric($feed['category'])) $extra = '?c='.$feed['category'];
+			else $extra = '';
+			echo "\t".'<link rel="alternate" type="application/rss+xml" title="'.$feed['title'].'" href="'.$config['core']['web_root'].'api/rss_'.$feed['name'].'.php'.$extra.'"/>'."\n";
+		}
+
+		foreach ($this->search as $search) {
+			echo '<link rel="search" type="application/opensearchdescription+xml" href="'.$search['url'].'" title="'.$search['name'].'"/>';
+		}
+
+		if ($this->favicon) {
+			echo '<link rel="icon" type="image/png" href="'.$this->favicon.'"/>';
+		}
+
+		$theme_dir = '';
+		if (!empty($config['my_themes'])) $theme_dir = $config['my_themes'];
+		else if (!empty($config['core']['web_root'])) $theme_dir = $config['core']['web_root'].'css/themes/';
+
+		if (!empty($h->session) && $theme_dir) {
+			echo '<link rel="stylesheet" type="text/css" href="'.$theme_dir.$h->session->theme.'"/>';
+		}
+
+		if (!empty($config['core']['web_root'])) {
+			echo '<script type="text/javascript" src="'.$config['core']['web_root'].'js/ajax.js"></script>';
+			echo '<script type="text/javascript" src="'.$config['core']['web_root'].'js/swfobject.js"></script>';
+			echo '<script type="text/javascript" src="'.$config['core']['web_root'].'js/functions.js"></script>';
+			echo '<script type="text/javascript" src="'.$config['core']['web_root'].'js/fileareas.js"></script>';
+			echo '<script type="text/javascript" src="'.$config['core']['web_root'].'js/chat_1on1.js"></script>';
+			echo '<script type="text/javascript" src="'.$config['core']['web_root'].'js/ext/prototype.js"></script>';
+			echo '<script type="text/javascript" src="'.$config['core']['web_root'].'js/ext/scriptaculous.js?load=builder,effects,dragdrop,controls,slider"></script>';
+			echo '<script type="text/javascript" src="'.$config['core']['web_root'].'js/ext/cropper.js"></script>';
+			if (!empty($files) && $files->allow_rating) {
+				echo '<script type="text/javascript" src="'.$config['core']['web_root'].'js/rate.js"></script>';
+			}
+		}
+
+		foreach ($this->js as $uri) {
+			echo '<script type="text/javascript" src="'.$uri.'"></script>';
+		}
+
+		echo '</head>';
+		if (count($this->onLoad)) {
+			echo '<body onload="';
+			foreach ($this->onLoad as $row) {
+				echo $row;
+			}
+			echo '">';
+		} else {
+			echo '<body>';
+		}
+
+		//XXX: remove getProjectPath eventually
+		if (function_exists('getProjectPath') && !empty($config['core']['web_root'])) {
+			echo '<script type="text/javascript">';
+			echo 'var _ext_ref="'.getProjectPath(2).'",_ext_core="'.$config['core']['web_root'].'api/";';
+			echo '</script>';
+		}
+
+	}
+
+}
+
+?>
