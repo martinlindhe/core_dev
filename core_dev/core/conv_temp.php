869f7e8708498effe4fff550abbefeca41dead5d
Martin Lindhe <martin@ubique.se>
2009-03-18 10:45:55 +0000

    comment cleanup

--- core_dev/core/conv_temp.php
+++ core_dev/core/conv_temp.php
@@ -4,26 +4,11 @@
  *
  * Conversion functions for different temperature systems
  *
- * Celcius (C)
- * -----------
- * 0°C and 100°C are arbitrarily placed at the melting and boiling
- * points of water and standard to the metric system.
- *
- * Farenheit (F)
- * -------------
- * The freezing point of water is 32 °F and the boiling point 212 °F,
- * placing the boiling and freezing points of water exactly 180 degrees apart.
- *
- * Kelvin (K)
- * ----------
- * An absolute temperature scale where absolute zero, the theoretical
- * absence of all thermal energy, is zero (0 K).
- *
- * Rankine (R)
- * -----------
- * As with the Kelvin scale, zero on the Rankine scale is absolute zero,
- * but the Rankine degree is defined as equal to one degree Fahrenheit,
- * rather than the one degree Celsius used by the Kelvin scale.
+ * Supported temperature systems:
+ * - Celcius (C)
+ * - Farenheit (F)
+ * - Kelvin (K)
+ * - Rankine (R)
  *
  * References
  * ----------
@@ -60,27 +45,27 @@ class temp
 		switch ($s) {
 			case 'C': return ($val * (9/5)) + 32;
 			case 'F': return  $val;
-			case 'K': return ($val * (9/5)) - 459.67;	//XXX verify!
-			case 'R': return  $val - 459.67;	//XXX verify!
+			case 'K': return ($val * (9/5)) - 459.67;
+			case 'R': return  $val - 459.67;
 		}
 	}
 
 	function toKelvin($s, $val)
 	{
 		switch ($s) {
-			case 'C': return  $val + 273.15; //XXX verify!
-			case 'F': return ($val + 459.67) * (5/9); //XXX verify
+			case 'C': return  $val + 273.15;
+			case 'F': return ($val + 459.67) * (5/9);
 			case 'K': return  $val;
-			case 'R': return  $val * (5/9); //XXX verifgy
+			case 'R': return  $val * (5/9);
 		}
 	}
 
 	function toRankine($s, $val)
 	{
 		switch ($s) {
-			case 'C': return ($val + 273.15) * (9/5); //XXX verify
-			case 'F': return  $val + 459.67; //XXX verify
-			case 'K': return  $val * (9/5); //XXX verify
+			case 'C': return ($val + 273.15) * (9/5);
+			case 'F': return  $val + 459.67;
+			case 'K': return  $val * (9/5);
 			case 'R': return  $val;
 		}
 	}
@@ -93,8 +78,8 @@ if ($t->conv('C', 'K', 300) != 573.15)    echo "FAIL 2\n";
 if ($t->conv('C', 'R', 300) != 1031.67)   echo "FAIL 3\n";
 
 if ($t->conv('F', 'C', 500) != 260)       echo "FAIL 4\n";
-if ($t->conv('F', 'K', 500) != 533.15)    echo "FAIL 5\n";
-if ($t->conv('F', 'R', 500) != 959.67)    echo "FAIL 6\n";
+if ($t->conv('F', 'K', 500) != 533.15)    echo "FAIL 5\n"; //XXX
+if ($t->conv('F', 'R', 500) != 959.67)    echo "FAIL 6\n"; //XXX
 
 if ($t->conv('K', 'C', 0) != -273.15)     echo "FAIL 7\n";
 if ($t->conv('K', 'F', 0) != -459.67)     echo "FAIL 8\n";
@@ -102,8 +87,8 @@ if ($t->conv('K', 'R', 0) != 0)           echo "FAIL 9\n";
 
 if ($t->conv('R', 'C', 509.67) != 10)     echo "FAIL 10\n";
 if ($t->conv('R', 'F', 509.67) != 50)     echo "FAIL 11\n";
-if ($t->conv('R', 'K', 509.67) != 283.15) echo "FAIL 12\n";
+if ($t->conv('R', 'K', 509.67) != 283.15) echo "FAIL 12\n"; //XXX
 
-//FIXME: some tests fail (5,6,12) however they return correct data. perhaps a datatype issue??
+//FIXME: some checks fail (5,6,12) but they return correct value. datatype issue??
 
 ?>
