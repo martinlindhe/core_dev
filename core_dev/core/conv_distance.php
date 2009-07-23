2119aee510e38c810672254a01f74fe3287bdcc5
Martin Lindhe <martin@ubique.se>
2009-07-23 21:38:28 +0000

    conv_currency.php: separated & cleaned up currency conversion class conv_distance.php: wip

---
+++ core_dev/core/conv_distance.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * $Id$
+ *
+ * Conversion functions for different distance systems
+ *
+ * Supported distance systems:
+ *  xxx
+ *  xxx
+ *
+ * References
+ * ----------
+ * xxx
+ *
+ * @author Martin Lindhe, 2009 <martin@startwars.org>
+ */
+
+class distance
+{
+	//XXX implement
+}
+
+?>
