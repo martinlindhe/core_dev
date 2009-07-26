dbb3bbd2977e0dfd8e8d3e3d0d3db41182d8086d
Martin Lindhe <martin@ubique.se>
2009-07-26 20:25:43 +0000

    remove debug text

--- core_dev/core/class.Cache.php
+++ core_dev/core/class.Cache.php
@@ -23,13 +23,11 @@ class cache
 	function get($key)
 	{
 		if ($this->handle->get($key.'_expiretime') < time()) return false;
-		echo "return cache\n";
 		return $this->handle->get($key);
 	}
 
 	function set($key, $val, $expire = 60)
 	{
-		echo "store\n";
 		$this->handle->set($key, $val);
 		$this->handle->set($key.'_expiretime', time()+$expire);
 	}
