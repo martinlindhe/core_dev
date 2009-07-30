e80a92895d2183fb96a096e5ef0beed78757dd4d
Martin Lindhe <martin@ubique.se>
2009-07-30 21:10:09 +0000

    added same interface to datasize conversion as the other conversion classes now have

--- core_dev/core/conv_datasize.php
+++ core_dev/core/conv_datasize.php
@@ -11,27 +11,54 @@
  * @author Martin Lindhe, 2009 <martin@startwars.org>
  */
 
-class mass
+class datasize
 {
 	var $scale = array( ///< unit scale to a bit
-	'bit'      => 1,
-	'kilobit'  => 1024,       // 2^10
-	'megabit'  => 1048576,    // 2^20
-	'gigabit'  => 1073741824, // 2^30
+	'bit'  => 1,
+	'kbit' => 1024,       // 2^10
+	'mbit' => 1048576,    // 2^20
+	'gbit' => 1073741824, // 2^30
 
-	'byte'     => 8,
-	'kilobyte' => 8192,            // (2^10)*8
-	'megabyte' => 8388608,         // (2^20)*8
-	'gigabyte' => 8589934592,      // (2^30)*8
-	'terabyte' => 8796093022208,   // (2^40)*8
-	'petabyte' => 9007199254740992,// (2^50)*8
+	'b'    => 8,
+	'kb'   => 8192,            // (2^10)*8
+	'mb'   => 8388608,         // (2^20)*8
+	'gb'   => 8589934592,      // (2^30)*8
+	'tb'   => 8796093022208,   // (2^40)*8
+	'pb'   => 9007199254740992,// (2^50)*8
 	);
 
 	function conv($from, $to, $val)
 	{
-		if (empty($this->scale[$from]) || empty($this->scale[$to])) return false;
+		$from = $this->shortcode($from);
+		$to   = $this->shortcode($to);
+		if (!$from || !$to) return false;
 
 		return ($val * $this->scale[$from]) / $this->scale[$to];
+	}
+
+	function shortcode($name)
+	{
+		if (substr($name, -1) == 's') $name = substr($name, 0, -1);
+
+		$lookup = array(
+		'bit'      => 'bit',
+		'kilobit'  => 'kbit',
+		'megabit'  => 'mbit',
+		'gigabit'  => 'gbit',
+
+		'byte'     => 'b',
+		'kilobyte' => 'kb',
+		'megabyte' => 'mb',
+		'gigabyte' => 'gb',
+		'terabyte' => 'tb',
+		'petabyte' => 'pb'
+		);
+
+		$name = strtolower($name);
+
+		if (!empty($lookup[$name])) return $lookup[$name];
+		if (array_search($name, $lookup)) return $name;
+		return false;
 	}
 }
 
--- core_dev/tests/test.conv_datasize.php
+++ core_dev/tests/test.conv_datasize.php
@@ -2,8 +2,8 @@
 
 require('/var/www/core_dev/core/conv_datasize.php');
 
-$m = new mass();
-if ($m->conv('megabyte', 'byte', 0.5) != 524288) echo "FAIL1\n";
-if ($m->conv('megabit', 'megabyte', 100) != 12.5) echo "FAIL2\n";
+$d = new datasize();
+if ($d->conv('megabyte', 'byte', 0.5) != 524288) echo "FAIL1\n";
+if ($d->conv('megabit', 'megabyte', 100) != 12.5) echo "FAIL2\n";
 
 ?>
