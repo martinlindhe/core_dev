9d2b9340726a27db1ca01b5b5e54c7479b806159
Martin Lindhe <martin@ubique.se>
2009-08-15 12:45:59 +0000

    input_feed.php: ATOM support input_http.php: uses curl for HTTPS support

--- core_dev/core/input_feed.php
+++ core_dev/core/input_feed.php
@@ -2,15 +2,16 @@
 /**
  * $Id$
  *
- * Simple RSS and ASX feed/playlist parser
+ * Simple RSS, ATOM and ASX feed/playlist parser
  * 
- * http://en.wikipedia.org/wiki/Advanced_Stream_Redirector
+ * RSS: http://en.wikipedia.org/wiki/Rss
+ * ATOM: http://en.wikipedia.org/wiki/Atom_(standard)
+ * ASX: http://en.wikipedia.org/wiki/Advanced_Stream_Redirector
  *
  * @author Martin Lindhe, 2008-2009 <martin@startwars.org>
  */
 
 //TODO: use input_xml for parsing
-//TODO: parse atom feeds
 
 //TODO: extend callback to allow mapping of special fields to standard ones, such as "<svtplay:xmllink svtplay:type="titles">http://xml.svtplay.se/v1/titles/102897</svtplay:xmllink>" to "guid"
 
@@ -20,6 +21,7 @@ class input_feed
 {
 	var $inside_item = false;
 	var $current_tag = '';
+	
 	var $link, $title, $desc, $pubDate, $guid;
 	var $attrs;
 	var $video_url, $video_type, $duration;
@@ -145,6 +147,7 @@ class input_feed
 
 	}
 
+
 	function parseRSS($data, $callback = '')
 	{
 		if (is_url($data)) {
@@ -169,8 +172,127 @@ class input_feed
 		if ($this->sort) uasort($this->entries, 'feed_sort_desc');
 		return $this->entries;
 	}
-	
 
+	function ATOMstartElement($parser, $name, $attrs = '')
+	{
+		$this->current_tag = $name;
+		$this->attrs = $attrs;
+
+		if ($this->current_tag == 'ENTRY') {
+			$this->inside_item = true;
+		}
+	}
+
+	function ATOMendElement($parser, $tagName, $attrs = '')
+	{
+		if ($tagName == 'LINK' && $this->inside_item) {
+			switch ($this->attrs['REL']) {
+			case 'alternate':
+				$this->link = $this->attrs['HREF'];
+				break;
+			case 'enclosure':
+				switch ($this->attrs['TYPE']) {
+				case 'video/x-flv':
+					$this->video_url  = $this->attrs['URL'];
+					$this->video_type = $this->attrs['TYPE'];
+					$this->duration   = $this->attrs['LENGTH'];
+					break;
+
+				case 'image/jpeg':
+					$this->image_url  = $this->attrs['URL'];
+					$this->image_type = $this->attrs['TYPE'];
+					break;
+
+				default:
+					echo "unknown enclosure mimetype: ".$this->attrs['TYPE']."\n";
+					die;
+					break;
+				}
+				break;
+			default:
+				echo "unknown link type: ".$this->attrs['REL']."\n";
+				die;
+			}
+		}
+		if ($tagName == 'ENTRY') {
+			$row['link']     = trim($this->link);
+			$row['title']    = html_entity_decode(trim($this->title), ENT_QUOTES, 'UTF-8');
+			$row['desc']     = html_entity_decode(trim($this->desc),  ENT_QUOTES, 'UTF-8');
+			$row['pubdate']  = strtotime(trim($this->pubDate));
+			$row['guid']     = trim($this->guid);
+			//$row['guid2']    = trim($this->guid2); //XXX remove hack!
+			$row['duration'] = trim($this->duration);
+			if ($this->video_url)  $row['video'] = $this->video_url;
+			if ($this->video_type) $row['video_type'] = $this->video_type;
+			if ($this->image_url)  $row['image'] = $this->image_url;
+			if ($this->video_type) $row['image_type'] = $this->image_type;
+
+			if ($this->callback) call_user_func($this->callback, $row);
+			$this->entries[] = $row;
+
+			$this->attrs = '';
+			$this->inside_item = false;
+
+			$this->link = '';
+			$this->title = '';
+			$this->desc = '';
+			$this->pubDate = '';
+			$this->guid = '';
+			//$this->guid2 = ''; //XXX rmeove hack!
+			$this->video_url  = '';
+			$this->video_type = '';
+			$this->duration   = 0;
+			$this->image_url  = '';
+			$this->image_type = '';
+		}
+	}
+
+	function ATOMcharacterData($parser, $data)
+	{
+		if (!$this->inside_item) return;
+		switch ($this->current_tag) {
+		case 'TITLE':
+			$this->title .= $data;
+			break;
+
+		case 'SUMMARY':
+			$this->desc .= $data;
+			break;
+
+		case 'UPDATED':
+			$this->pubDate .= $data;
+			break;
+
+		case 'ID':
+			$this->guid .= $data;
+			break;
+		}
+	}
+
+	function parseATOM($data, $callback = '')
+	{
+		if (is_url($data)) {
+			$u = new url_handler($data);
+			$data = $u->fetch();
+		}
+
+		$this->entries = array();
+
+		$parser = xml_parser_create();
+		xml_set_object($parser, $this);
+		xml_set_element_handler($parser, 'ATOMstartElement', 'ATOMendElement');
+		xml_set_character_data_handler($parser, 'ATOMcharacterData');
+
+		if (function_exists($callback)) $this->callback = $callback;
+
+		if (!xml_parse($parser, $data, true)) {
+			echo "parseATOM XML error: ".xml_error_string(xml_get_error_code($parser))." at line ".xml_get_current_line_number($parser)."\n";
+		}
+		xml_parser_free($parser);
+
+		if ($this->sort) uasort($this->entries, 'feed_sort_desc');
+		return $this->entries;
+	}
 
 	function ASXstartElement($parser, $name, $attrs = '')
 	{
@@ -219,14 +341,30 @@ class input_feed
 		}
 
 	}
+	
+	function fetch($url, $callback = '')
+	{
+		if (!is_url($url)) return false;
+
+		$u = new url_handler($url);
+		$data = $u->get();
+
+		if (strpos($data, '<rss ') !== false) {
+			return $this->parseRSS($data, $callback);
+		} else if (strpos($data, '<asx ') !== false) {
+			return $this->parseASX($data, $callback);
+		} else if (strpos($data, '<feed ') !== false) {
+			return $this->parseATOM($data, $callback);
+		}
+
+		echo "ERROR: unhandled feed: ".substr($data, 0, 200)." ...\n";
+		die;
+	
+	}
+	
 
 	function parseASX($data, $callback = '')
 	{
-		if (is_url($data)) {
-			$u = new url_handler($data);
-			$data = $u->fetch();
-		}
-
 		$this->entries = array();
 
 		$parser = xml_parser_create();
--- core_dev/core/input_http.php
+++ core_dev/core/input_http.php
@@ -19,12 +19,19 @@ require_once('class.Cache.php');
 class url_handler
 {
 	var $scheme, $host, $port, $path, $param;
+	var $headers;
 	var $cache;
 	var $cache_time = 300; //5min
 	var $debug = false;
 
 	function __construct($url = '')
 	{
+		echo phpversion('curl');
+		if (!function_exists('curl_exec')) {
+			echo "ERROR: php5-curl missing\n";
+			//return false;
+		}
+
 		if ($url) $this->parse($url);
 
 		$this->cache = new cache();
@@ -35,6 +42,7 @@ class url_handler
 		$parsed = parse_url($url);
 		switch ($parsed['scheme']) {
 		case 'http':
+		case 'https':
 		case 'rtmp':
 			break;
 		default:
@@ -71,17 +79,13 @@ class url_handler
 	/**
 	 * Fetches the data of the web resource
 	 */
-	function fetch($cache_time = false)
+	function get($cache_time = false)
 	{
 		$url = $this->compact();
-		$key = 'url//'.htmlspecialchars($url);
 
-		$data = $this->cache->get($key);
-		if (!$data) {
-			if ($this->debug) echo "REAL READ ".$url."\n";
-			$data = file_get_contents($url);
-			$this->cache->set($key, $data, ($cache_time !== false ? $cache_time : $this->cache_time));
-		} else if ($this->debug) echo "CACHE READ ".$url."\n";
+		$data = http_get($url, false, $cache_time);
+		$this->headers = http_head($url, $cache_time);
+
 		return $data;
 	}
 
@@ -130,12 +134,11 @@ function is_url($url)
 }
 
 /**
- * Fetches header for given URL and returns it in a parsed array
- *
- * @param $url URL to request HTTP headers for
- * @return array of parsed headers or false on error
+ * HTTP/HTTPS GET function, using curl
+ * works like "file_get_contents()"
+ * @param $head if true, return HTTP HEADer, else the BODY
  */
-function http_head($url)
+function http_get($url, $head_only = false, $cache_time = 60)
 {
 	global $config;
 
@@ -144,45 +147,84 @@ function http_head($url)
 		return false;
 	}
 
+	$cache = new cache();
+	$key_head = 'url_head//'.htmlspecialchars($url);
+	$key_body = 'url//'.htmlspecialchars($url);
+		
 	$u = parse_url($url);
 
 	switch ($u['scheme']) {
-		case 'http':
-			$default_port = 80;
-			break;
+	case 'http':
+		$default_port = 80;
+		break;
 
-		//FIXME https support (?)
-		default:
-			echo "unsupported url scheme: ".$u['scheme']."\n";
-			return;
+	case 'https':
+		$default_port = 443;
+		break;
+
+	default:
+		echo "http_head() unsupported url scheme: ".$u['scheme']."\n";
+		return;
 	}
 
 	if (empty($u['port'])) $u['port'] = $default_port;
 
-	$fp = fsockopen($u['host'], $u['port'], $errno, $errstr, 30);
-	if (!$fp) {
-		echo "$errstr ($errno)\n";
+			
+	$ch = curl_init($url);
+	if (!$ch) {
+		echo "curl error: ".curl_errstr($ch)." (".curl_errno($ch).")\n";
 		return false;
 	}
-
-	$query_header  = "HEAD ".$u['path'].(!empty($u['query']) ? '?'.$u['query'] : '')." HTTP/1.1\r\n";
-	$query_header .= "Host: ".$u['host']."\r\n";
-	$query_header .= "User-Agent: ".$config['http']['user_agent']."\r\n";	//XXX: core_dev + version?
-	$query_header .= "Connection: close\r\n\r\n";
-	fwrite($fp, $query_header);
-
-	$result = '';
-	while (!feof($fp)) {
-		$result .= fgets($fp, 512);
+	
+	curl_setopt($ch, CURLOPT_USERAGENT, $config['http']['user_agent']);
+	
+	if ($head_only) {
+		$headers = $cache->get($key_head);
+		if ($headers)
+			return unserialize($headers);
+	} else {
+		$body = $cache->get($key_body);
+		if ($body)
+			return $body;
 	}
-	fclose($fp);
+
+	curl_setopt($ch, CURLOPT_HEADER, 1);
+	curl_setopt($ch, CURLOPT_NOBODY, 0);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+
+	if ($u['scheme'] == 'https') {
+		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+	}
+
+	$result = curl_exec($ch);
+	curl_close($ch);
 
 	//Cut off header
 	$pos = strpos($result, "\r\n\r\n");
 	$result_header = substr($result, 0, $pos);
+	$result_body   = substr($result, $pos + strlen("\r\n\r\n"));
 
 	$headers = explode("\r\n", $result_header);
-	return $headers;
+	$cache->set($key_head, serialize($headers), $cache_time);
+	$cache->set($key_body, $result_body, $cache_time);
+
+	if ($head_only)
+		return $headers;
+
+	return $result_body;
+}
+
+/**
+ * Fetches header for given URL and returns it in a parsed array
+ *
+ * @param $url URL to request HTTP headers for
+ * @oaram $cache_time seconds to cache result, 0 to disable
+ * @return array of parsed headers or false on error
+ */
+function http_head($url, $cache_time = 60)
+{
+	return http_get($url, true, $cache_time);
 }
 
 /**
@@ -197,9 +239,9 @@ function http_status($p)
 
 	foreach ($p as $h) {
 		switch (substr($h, 0, 9)) {
-			case 'HTTP/1.0 ':
-			case 'HTTP/1.1 ':
-				return intval(substr($h, 9));
+		case 'HTTP/1.0 ':
+		case 'HTTP/1.1 ':
+			return intval(substr($h, 9));
 		}
 	}
 	return false;
@@ -217,9 +259,9 @@ function http_last_modified($p)
 
 	foreach ($p as $h) {
 		$col = explode(': ', $h);
-		switch ($col[0]) {
-			case 'Last-Modified':
-				return strtotime($col[1]);
+		switch (strtolower($col[0])) {
+		case 'last-modified':
+			return strtotime($col[1]);
 		}
 	}
 	return false;
@@ -237,12 +279,34 @@ function http_content_length($p)
 
 	foreach ($p as $h) {
 		$col = explode(': ', $h);
-		switch ($col[0]) {
-			case 'Content-Length':
-				return intval($col[1]);
+		switch (strtolower($col[0])) {
+		case 'content-length':
+			return intval($col[1]);
 		}
 	}
 	return 0;
+}
+
+/**
+ * Returns the Content-Type field from given URL
+ *
+ * @param $p is a URL or an array from http_head()
+ * @return content length
+ */
+function http_content_type($p)
+{
+	if (!is_array($p)) $p = http_head($p);
+
+	foreach ($p as $h) {
+		$col = explode(': ', $h);
+		switch (strtolower($col[0])) {
+		case 'content-type':
+			//text/xml;charset=UTF-8 
+			$param = explode(';', $col[1]);
+			return $param[0];
+		}
+	}
+	return false;
 }
 
 ?>
