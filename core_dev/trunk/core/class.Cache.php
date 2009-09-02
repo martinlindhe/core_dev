521ec4d680e5b574ef36bf36bf6e18f4e0c5a58c
Martin Lindhe <martin@ubique.se>
2009-09-02 20:01:56 +0000

    implemented support for multiple cache servers in the cache class

--- core_dev/trunk/core/class.Cache.php
+++ core_dev/trunk/core/class.Cache.php
@@ -4,13 +4,11 @@
  *
  * Implements a cache using memcached with automatic expire time
  *
- * for memcached: "memcached php5-memcache"
- * for xcache: "php5-xcache"
+ * Requirements: memcached php5-memcache
  *
  * @author Martin Lindhe, 2009 <martin@startwars.org>
  */
 
-//XXX: use xcache's cache instead if its available
 //XXX: add "inc/dec" to increase/Decrease numeric values
 
 class cache
@@ -19,44 +17,46 @@ class cache
 	var $debug  = false;
 	var $mode   = '';
 
-	function __construct($server = '127.0.0.1', $port = 11211)
+	function __construct($server_pool = false)
 	{
-		/*if (function_exists('xcache_get')) {
-			$this->mode = 'xcache';
-			return true;
-		} else */if (class_exists('Memcache')) {
-			$this->mode = 'memcache';
-			$this->handle = new Memcache;
-			$this->handle->connect($server, $port);
-			return true;
+		if (!class_exists('Memcache')) {
+			echo "cache FAIL: php5-xcache or php5-memcache not found\n";
+			return false;
 		}
-		echo "cache FAIL: php5-xcache or php5-memcache not found\n";
-		return false;
+
+		$this->mode = 'memcache';
+		$this->handle = new Memcache;
+
+		if (!$server_pool) $server_pool = array('127.0.0.1:11211');
+
+		foreach ($server_pool as $server) {
+
+			$ex = explode(':', $server);
+			if (empty($ex[1])) $ex[1] = 11211;
+			list($host, $port) = $ex;
+
+			$this->handle->addServer($host, $port, true);
+		}
+
+		return true;
 	}
 
 	function get($key)
 	{
-		if ($this->mode == 'xcache') {
-			$val = xcache_get($key); //XXX i never get data returned when testing 2009.08.15
-		} else {
-			if (!$this->handle) return false;
+		if (!$this->handle) return false;
 
-			$val = $this->handle->get($key);
-		}
+		$val = $this->handle->get($key);
+
 		if ($this->debug) echo "CACHE READ ".$key." = ".$val."\n";
-
 		return $val;
 	}
 
 	function set($key, $val, $expire = 60)
 	{
-		if ($this->mode == 'xcache') {
-			$ret = xcache_set($key, $val, $expire);
-		} else {
-			if (!$this->handle) return false;
+		if (!$this->handle) return false;
 
-			$ret = $this->handle->set($key, $val, false, $expire);
-		}
+		$ret = $this->handle->set($key, $val, false, $expire);
+
 		if ($this->debug) echo "CACHE WRITE ".$key." = ".$val." (".$expire." sec)\n";
 		return $ret;
 	}
--- core_dev/trunk/tests/test.class.Cache.php
+++ core_dev/trunk/tests/test.class.Cache.php
@@ -1,12 +1,18 @@
 <?php
 
-require_once('/var/www/core_dev/core/class.Cache.php');
+require_once('/var/www/core_dev/trunk/core/class.Cache.php');
 
-$cache = new Cache();
+$pool = array(
+'127.0.0.1',
+'192.168.77.220:27788'
+);
+
+$cache = new Cache($pool);
 
 $x = $cache->get('coredev_cache_test');
-if ($x) echo "CACHED: ".$x."\n";
-else {
+if ($x) {
+	echo "CACHED: ".$x."\n";
+} else {
 	$cache->set('coredev_cache_test', time());
 	echo "STORED\n";
 }
