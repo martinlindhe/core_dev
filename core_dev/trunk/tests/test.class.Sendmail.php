6aa32dc7d4b74e13a1d8e3660fb66994af1ad90c
Martin Lindhe <martin@ubique.se>
2009-09-03 10:58:28 +0000

    updated test code

--- core_dev/trunk/tests/test.class.Sendmail.php
+++ core_dev/trunk/tests/test.class.Sendmail.php
@@ -6,22 +6,24 @@ $config['debug'] = true;
 
 $mail = new Sendmail('mail.startwars.org', 'martintest@startwars.org', 'test111');	//postfix
 
-$mail->from('martin@startwars.org', "Måartin Lindhe");
-$mail->to('martin@unicorn.se');
-$mail->reply_to("noreply@unicorn.se", "iåenget svar");
+$mail->setFrom('martin@startwars.org', "Måartin Lindhe");
+$mail->setReplyTo("noreply@unicorn.se", "iåenget svar");
+
+$mail->setSubject('message åäö subject');
+$mail->addRecipient('martin@unicorn.se');
 
 /*
 $mail->embed('delicious.png', 'pic_name');
 $mail->attach('delicious.png');
 */
 
-//$mail->html = true;
+//$mail->setHTML(true);
 
 $msg ='abc åäö 123';
 
 //$msg = '<h1>HEJ HEJ HEJ</h1><br><br><img src="cid:pic_name"><font color="red">lalala</font>';
 
-$mail->send('message åäö subject', $msg);
+$mail->send($msg);
 
 ?>
 
