2d30f4471dafef7340890f0eadf37cb602788541
Martin Lindhe <martin@ubique.se>
2009-09-08 23:36:26 +0000

    * class.Weather.php renamed to client_weather.php * weather: made cache a private object * service_weather_webservicex: made a class interface and added getCitiesByCountry() function

---
+++ core_dev/trunk/core/client_weather.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * $Id$
+ *
+ * Client library to query web services about weather reports
+ *
+ * @author Martin Lindhe, 2009 <martin@startwars.org>
+ */
+
+require_once('class.Cache.php');
+require_once('service_weather_webservicex.php');
+
+class weather
+{
+	private $cache;
+
+	function __construct()
+	{
+		$this->cache = new cache();
+		//$this->cache->debug = true;
+		$this->cache->setCacheTime(60*60); //default to 60 minute cache
+	}
+
+	function setCacheTime($s) { $this->cache->setCacheTime($s); }
+
+	function getWeatherReport($city, $country = '')
+	{
+		$city    = strtolower($city);
+		$country = strtolower($country);
+
+		$data = $this->cache->get('weather//'.$city.'/'.$country);
+		if ($data) return unserialize($data);
+
+		$client = new weather_webservicex();
+		$res = $client->getWeather($city, $country);
+
+		$this->cache->set('weather//'.$city.'/'.$country, serialize($res));
+		return $res;
+	}
+}
+
+?>
--- core_dev/trunk/core/service_weather_webservicex.php
+++ core_dev/trunk/core/service_weather_webservicex.php
@@ -3,53 +3,111 @@
  * $Id$
  *
  * Gets current weather for major cities around the world
+ * Use client_weather.php for a general API
+ *
  * http://www.webservicex.net/WCF/ServiceDetails.aspx?SID=48
  */
 
 require_once('conv_temp.php');
 require_once('input_xml.php');
 
-define('WEBSERVICEX_WEATHER_API', 'http://www.webservicex.net/globalweather.asmx?wsdl');
-
-function webservicex_weather($city, $country = '')
+class weather_webservicex
 {
-	$client = new SoapClient(WEBSERVICEX_WEATHER_API);
+	private $client;
 
-	try {
-		$params['CityName']    = $city;
-		$params['CountryName'] = $country;
-		$val = $client->GetWeather($params);
-		$xml = $val->GetWeatherResult;
-		if ($xml == 'Data Not Found') {
-			echo "webservicex_weather() data fetch failed on city=".$city.", country=".$country."\n";
+	function __construct()
+	{
+		$this->client = new SoapClient('http://www.webservicex.net/globalweather.asmx?wsdl');
+	}
+
+	function getCitiesByCountry($country)
+	{
+		$params = array(
+		'CountryName' => $country
+		);
+
+		try {
+			$val = $this->client->GetCitiesByCountry($params);
+			$xml = $val->GetCitiesByCountryResult;
+			if ($xml == 'Data Not Found') {
+				echo "webservicex_weather::getCitiesByCountry() not found on country=".$country.dln();
+				return false;
+			}
+
+			//XXX parse it properly
+			d($xml);
+			return $xml;
+
+		} catch (Exception $e) {
+			echo 'exception: '.$e, "\n";
+			echo 'Request header:'.$this->client->__getLastRequestHeaders()."\n";
+			echo 'Request: '.$this->client->__getLastRequest()."\n";
+			echo 'Response: '. $this->client->__getLastResponse()."\n";
+			return false;
+		}
+	}
+
+	function getWeather($city, $country = '')
+	{
+		$params = array(
+		'CountryName' => $country,
+		'CityName'    => $city
+		);
+
+		try {
+			$val = $this->client->GetWeather($params);
+			$xml = $val->GetWeatherResult;
+
+			if ($xml == 'Data Not Found') {
+				echo "webservicex_weather::getWeather() not found on city=".$city.", country=".$country.dln();
+				return false;
+			}
+
+			$x = new xml_input();
+			$p = $x->parse($xml);
+			if (!$p) return false;
+
+			$celcius = false;
+			if (!empty($p['CurrentWeather|Temperature'])) {
+				list($farenheit) = explode(' ', $p['CurrentWeather|Temperature']);
+				$temp = new temperature();
+				$celcius = round($temp->conv('F','C', $farenheit), 1);
+			}
+
+			//parse "Sundsvall-Harnosand Flygplats, Sweden (ESNN) 62-32N 017-27E 10M"
+			list($location, $rest) = explode('(', $p['CurrentWeather|Location']);
+			list($code, $coords) = explode(')', $rest);
+			//echo 'CODE: '.$code."\n";   //ESNQ, ESSB, ESNN.... what is this?
+
+			//parse "Jul 28, 2009 - 02:20 PM EDT / 2009.07.28 1820 UTC"
+			list($time1, $time2) = explode(' / ', $p['CurrentWeather|Time']);
+
+			//echo "t1: ".$time1."<br>"; echo "t2: ".$time2."<br>";
+			$time = strtotime($time1); //FIXME strtotime() dont handle either format
+
+			$res = array(
+			'Location'     => trim($location),
+			'Coordinates'  => trim($coords), //XXX: convert from "59-21N 017-57E 14M"
+			'Time'         => $time,
+			'Wind'         =>@$p['CurrentWeather|Wind'],         //XXX: "from the SSE (150 degrees) at 5 MPH (4 KT):0"
+			'Visibility'   =>@$p['CurrentWeather|Visibility'],   //XXX: "greater than 7 mile(s):0"
+			'SkyConditions'=>@$p['CurrentWeather|SkyConditions'],//see $skyconditions_swe (can be empty)
+			'Temperature'  => $celcius
+			);
+
+			return $res;
+
+		} catch (Exception $e) {
+			echo 'exception: '.$e, "\n";
+			echo 'Request header:'.$this->client->__getLastRequestHeaders()."\n";
+			echo 'Request: '.$this->client->__getLastRequest()."\n";
+			echo 'Response: '. $this->client->__getLastResponse()."\n";
 			return false;
 		}
 
-		$x = new xml_input();
-		$p = $x->parse($xml);
-		if (!$p) return false;
-
-		list($farenheit) = explode(' ', $p['CurrentWeather|Temperature']);
-		$temp = new temp();
-		$celcius = round($temp->conv('F','C', $farenheit), 1);
-
-		$res = array(
-		'Location'     =>@$p['CurrentWeather|Location'],     //"Stockholm / Bromma, Sweden (ESSB) 59-21N 017-57E 14M"
-		'Time'         =>@$p['CurrentWeather|Time'],         //XXX: "Jul 28, 2009 - 02:20 PM EDT / 2009.07.28 1820 UTC"
-		'Wind'         =>@$p['CurrentWeather|Wind'],         //XXX: "from the SSE (150 degrees) at 5 MPH (4 KT):0"
-		'Visibility'   =>@$p['CurrentWeather|Visibility'],   //XXX: "greater than 7 mile(s):0"
-		'SkyConditions'=>@$p['CurrentWeather|SkyConditions'],//see $skyconditions_swe (can be empty)
-		'Temperature'  => $celcius
-		);
-		return $res;
-
-	} catch (Exception $e) {
-		echo 'exception: '.$e, "\n";
-		echo 'Request header:'.$client->__getLastRequestHeaders()."\n";
-		echo 'Request: '.$client->__getLastRequest()."\n";
-		echo 'Response: '. $client->__getLastResponse()."\n";
-		return false;
 	}
 }
+
+
 
 ?>
