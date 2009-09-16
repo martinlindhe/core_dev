9e7a6b68b06077c7efe5b6a49ebd7076d4796e97
Martin Lindhe <martin@ubique.se>
2009-09-16 11:50:31 +0000

    added test script

---
+++ core_dev/trunk/tests/test.io_ini.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once('/var/www/core_dev/trunk/core/io_ini.php');
+
+$x = new ini('test.ini');
+
+$x->set('Category', 'spex', 17 );
+
+$val = $x->get('Category', 'spex');
+
+echo "category->val: ".$val."\n";
+
+
+?>
