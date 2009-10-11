95020a11077d7a2163671e8fd3b86f7aa83a7536
Martin Lindhe <martin@ubique.se>
2009-10-11 13:21:48 +0000

    category class is now working as intended, i hope

--- core_dev/trunk/core/class.Category.php
+++ core_dev/trunk/core/class.Category.php
@@ -3,21 +3,36 @@
  * $Id$
  */
 
+//STATUS: satisfied
+
+//TODO: some general methods for ui category management (edit, remove)
+
 require_once('constants.php');
 
 class CategoryItem
 {
 	private $tbl_name;     ///< table name
 	private $name;
+	private $id;
 	private $type;         ///< type as defined in constants.php
 	private $owner;
 	private $creator;      ///< if set, stores creatorId when categories are created
 
 	private $permissions = PERM_USER;  ///< permission flags as defined in constants.php
 
+	function __construct($type)
+	{
+		global $h;
+		if (!is_numeric($type)) return false;
+
+		$this->type     = $type;
+		$this->tbl_name = 'tblCategories';
+	}
+
 	function setTableName($name) { $this->tbl_name = $name; }
 
 	function setName($name) { $this->name = $name; }
+	function setId($id) { $this->id = $id; }
 
 	function setType($id)
 	{
@@ -43,12 +58,13 @@ class CategoryItem
 		$this->permissions = $flags;
 	}
 
-	function getName($id)
+	function getId() { return $this->id; }
+
+	function getName()
 	{
 		global $db;
-		if (!is_numeric($id)) return false;
 
-		$q = 'SELECT categoryName FROM '.$this->tbl_name.' WHERE categoryId='.$id.' AND categoryType='.$this->type.' ';
+		$q = 'SELECT categoryName FROM '.$this->tbl_name.' WHERE categoryId='.$this->id.' AND categoryType='.$this->type.' ';
 		if ($this->owner) $q .= 'AND ownerId='.$this->owner;
 		return $db->getOneItem($q);
 	}
@@ -65,14 +81,15 @@ class CategoryItem
 		$q = 'SELECT categoryId FROM '.$this->tbl_name.' WHERE categoryType='.$this->type.' AND categoryName="'.$db->escape($this->name).'"';
 		if ($this->owner) $q .= ' AND ownerId='.$this->owner;
 		if ($this->creator) $q .= ' AND creatorId='.$this->creator;
-		$id = $db->getOneItem($q);
-		if ($id) return $id;
+		$this->id = $db->getOneItem($q);
+		if ($this->id) return $this->id;
 
 		$q = 'INSERT INTO '.$this->tbl_name.' SET categoryType='.$this->type.',categoryName="'.$db->escape($this->name).'"';
 		$q .= ',timeCreated=NOW(),permissions='.$this->permissions;
 		if ($this->owner) $q .= ',ownerId='.$this->owner;
 		if ($this->creator) $q .= ',creatorId='.$this->creator;
-		return $db->insert($q);
+		$this->id = $db->insert($q);
+		return $this->id;
 	}
 }
 
@@ -142,7 +159,7 @@ class CategoryList
 		foreach ($this->getList() as $cat_id => $cat_name)
 			$res .= ', <a href="?cat='.$cat_id.'">'.$cat_name.'</a>';
 
-		return substr($res, 2);
+		return '<a href="?cat=0">'.t('Overview').'</a>, '.substr($res, 2);
 	}
 
 }
