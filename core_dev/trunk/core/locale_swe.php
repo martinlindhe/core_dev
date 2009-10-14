1d1ed9cf33e2c7d52560d5e4a476088f60abe453
Martin Lindhe <martin@ubique.se>
2009-10-14 08:12:49 +0000

    $month_swe_short added, with 3-letter month names

--- core_dev/trunk/core/locale_swe.php
+++ core_dev/trunk/core/locale_swe.php
@@ -11,6 +11,9 @@ $month_swe = array(
 '', 'Januari', 'Februari', 'Mars', 'April', 'Maj', 'Juni',
 'Juli', 'Augusti', 'September', 'Oktober', 'November', 'December');
 
+$month_swe_short = array('', 'Jan', 'Feb', 'Mar', 'Apr', 'Maj', 'Jun', 'Jul', 'Aug', 'Sep', 'Okt', 'Nov', 'Dec');
+
+
 $weekday_swe = array(
 '', 'Måndag', 'Tisdag', 'Onsdag', 'Torsdag', 'Fredag', 'Lördag', 'Söndag');
 
