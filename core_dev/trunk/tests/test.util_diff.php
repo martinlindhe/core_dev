9d2f225f1a0c8added1b7f4e4dc73e0aef4d68d6
Martin Lindhe <martin@ubique.se>
2009-10-25 16:42:15 +0000

    renamed output_diff.php to util_diff.php

--- core_dev/trunk/tests/test.util_diff.php
+++ core_dev/trunk/tests/test.util_diff.php
@@ -2,7 +2,7 @@
 
 set_include_path(get_include_path() . PATH_SEPARATOR . dirname(__FILE__).'/../core/');
 
-require_once('output_diff.php');
+require_once('util_diff.php');
 
 $x1 = "din mamma heter kallops\nDin med!\n";
 $x2 = "min mamma heter kallops\nDin med!\n";
