399c74758d05feb5ee7d1d4741f3bfb3adf86306
Martin Lindhe <martin@ubique.se>
2009-11-02 19:18:06 +0000

    updated Timestamp usage

--- projects/validators/timestamp/index.php
+++ projects/validators/timestamp/index.php
@@ -7,20 +7,20 @@
 
 require_once('config.php');
 
-require_once('class.Timestamp.php');
+require_once('prop_Timestamp.php');
 
 $ts = new Timestamp(1200685768);
 
 if (!empty($_POST['ts'])) {
 	$ts->set($_POST['ts']);
 
-	echo 'Unix timestamp (RFC 2822): '.date('r', $ts->getUnix()).'<br/>';
+	echo 'Unix timestamp (RFC 2822): '.date('r', $ts->get()).'<br/>';
 	echo 'NTP timestamp (RFC 2822): '.date('r', $ts->getNTP() ).'<br/>';
 	echo '<br/>';
 }
 ?>
 
 <form method="post" action="">
-Timestamp: <input type="text" name="ts" size="15" value="<?php echo $ts->getUnix()?>"/>
+Timestamp: <input type="text" name="ts" size="15" value="<?php echo $ts->get()?>"/>
 <input type="submit" class="button" value="Convert"/>
 </form>
