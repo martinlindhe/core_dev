fe9cf32257089eecf1d2b6b9a78b6a2fc1e49f74
Martin Lindhe <martin@ubique.se>
2010-04-08 20:55:51 +0000

    added a numeral system converter

---
+++ core_dev/trunk/core/conv_numeral.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * $Id$
+ *
+ * Converter between different numeral systems
+ *
+ * http://en.wikipedia.org/wiki/Radix
+ * http://en.wikipedia.org/wiki/Numeral_system
+ *
+ * @author Martin Lindhe, 2010 <martin@startwars.org>
+ */
+
+//TODO: handle bases above 10 (at least hex)
+
+require_once('class.CoreConverter.php');
+
+class ConvertNumeral extends CoreConverter
+{
+    private $scale = array( ///< unit scale to a second
+    'bin' => 2,
+    'oct' => 8,
+    'dec' => 10,
+    //'hex' => 16,
+    );
+
+    private $lookup = array(
+    'binary'      => 'bin',
+    'octal'       => 'oct',
+    'decimal'     => 'dec',
+    //'hexadecimal' => 'hex',
+    );
+
+    function getShortcode($name)
+    {
+        $name = strtolower($name);
+        if (substr($name, -1) == 's') $name = substr($name, 0, -1);
+
+        if (!empty($this->lookup[$name])) return $this->lookup[$name];
+        if (array_search($name, $this->lookup)) return $name;
+        return false;
+    }
+
+    function conv($from, $to, $val)
+    {
+        $from = $this->getShortcode($from);
+        $to   = $this->getShortcode($to);
+        if (!$from || !$to) return false;
+
+        $base_from = $this->scale[$from];
+        $base_to   = $this->scale[$to];
+
+        //XXX assumes base 2 to 10
+        $res = $val % $base_to;
+
+        $multiplier = $base_from;
+
+        while (($val = intval($val / $base_to)) > 0)
+        {
+            $res += ($val % $base_to) * $multiplier;
+            $multiplier *= $base_from;
+        }
+
+        return $res;
+    }
+
+}
+
+?>
---
+++ core_dev/trunk/tests/test.conv_numeral.php
@@ -0,0 +1,15 @@
+<?php
+
+set_include_path(get_include_path() . PATH_SEPARATOR . dirname(__FILE__).'/../core/');
+
+require('conv_numeral.php');
+
+$d = new ConvertNumeral();
+
+if ($d->conv('decimal', 'binary', '11') != '1011') echo "FAIL 1\n";
+if ($d->conv('decimal', 'octal', '44')  != '54')   echo "FAIL 2\n";
+if ($d->conv('octal', 'decimal', '33')  != '27')   echo "FAIL 3\n";
+if ($d->conv('octal', 'decimal', '1234') != '668') echo "FAIL 4\n";
+if ($d->conv('binary', 'decimal', '1110') != '14') echo "FAIL 5\n";
+
+?>
