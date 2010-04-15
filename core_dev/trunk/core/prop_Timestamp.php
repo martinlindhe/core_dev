a3b5596e9581efdac738f554318462f7fa84e8df
Martin Lindhe <martin@ubique.se>
2010-04-15 19:38:54 +0000

    prop_Version: WIP, unused

--- core_dev/trunk/core/prop_Timestamp.php
+++ core_dev/trunk/core/prop_Timestamp.php
@@ -125,8 +125,7 @@ class Timestamp extends CoreProperty
 
     function render()
     {
-        //XXX maybe call locale-specific functions to handle rendering
-
+        //FIXME translate months according to current language
         $months = array(
         1=>'January',    2=>'February', 3=>'March',     4=>'April',
         5=>'May',        6=>'June',     7=>'July',      8=>'August',
---
+++ core_dev/trunk/core/prop_Version.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * $Id$
+ *
+ * @author Martin Lindhe, 2010 <martin@startwars.org>
+ */
+
+//STATUS: wip, unused
+
+require_once('core.php');
+require_once('class.CoreProperty.php');
+
+class Version extends CoreProperty
+{
+    private $major;
+    private $minor;
+    private $micro;
+    private $raw; ///< raw input
+
+    private $type  = 0;
+    const VERSION  = 1; //v1.5.390a
+    const REVISION = 2; //r1234
+
+    function get()
+    {
+        return $this->render();
+    }
+
+    function set($s)
+    {
+        if (!$s) return;
+
+        $this->raw = $s;
+
+        //XXX regexp match for "r123"
+        $this->type = self::VERSION;
+
+        //XXX assuming "1.2.3" format
+        $x = explode('.', $s, 3);
+        $this->major = $x[0];
+
+        if (isset($x[1]))
+            $this->minor = $x[1];
+
+        if (isset($x[2]))
+            $this->micro = $x[2];
+    }
+
+    function render()
+    {
+        switch ($this->type) {
+        case self::VERSION:
+            return $this->major.
+                ($this->minor || $this->micro ? '.'.$this->minor : '').
+                ($this->micro ? '.'.$this->micro : '');
+        default:
+            return 'Version: cant render "'.$this->raw.'"';
+        }
+    }
+
+}
+
+/*
+$x = new Version('4.0.32.233');
+echo $x->get().ln();
+
+*/
+?>
