f3c7c86d2ae91814bfaffa354099d56dad683302
Linus Wiklund <linus@unicorn.se>
2010-04-27 15:50:53 +0000

    Class to interface with MSSQL-databases, WIP

--- core_dev/trunk/core/sql_mssql.php
+++ core_dev/trunk/core/sql_mssql.php
@@ -2,23 +2,47 @@
 /**
  * $Id$
  *
- * MySQL db driver using the php_mysqli extension
+ * MSSQL db driver using the php_mssql extension
  *
- * @author Martin Lindhe, 2007-2009 <martin@startwars.org>
+ * @author Martin Lindhe, 2007-2010 <martin@startwars.org>
  */
 
-//STATUS: dont work. need rewrite, see sql_mysql.php
+require_once('class.CoreBase.php');
+require_once('ISql.php');
 
-require_once('db_base.php');
+//STATUS: wip
+//TODO: update getNumArray()
 
-class db_mssql extends db_base
+//TODO: rewrite using PHP Data Objects: http://se.php.net/pdo
+
+class DatabaseMssql extends CoreBase implements IDB_SQL
 {
-    /**
-     * Destructor
-     */
-    function __destruct()
+    var $db_handle       = false;       ///< Internal db handle
+    var $host            = 'localhost'; ///< Hostname or numeric IP address of the db server
+    var $port            = 1433;        ///< Port number
+    var $username        = 'root';      ///< Username to use to connect to the database
+    var $password        = '';          ///< Password to use to connect to the database
+    var $database        = '';          ///< Name of the database to connect to
+    var $charset         = 'utf8';      ///< What character set to use
+    protected $connected = false;       ///< Are we connected to the db?
+
+    function setConfig($conf)
     {
-        if ($this->connected) mssql_close($this->db_handle);
+        if (!is_array($conf))
+            return;
+
+        if (!empty($conf['host']))     $this->host     = $conf['host'];
+        if (!empty($conf['port']))     $this->port     = $conf['port'];
+        if (!empty($conf['username'])) $this->username = $conf['username'];
+        if (!empty($conf['password'])) $this->password = $conf['password'];
+        if (!empty($conf['database'])) $this->database = $conf['database'];
+        if (!empty($conf['charset']))  $this->charset  = $conf['charset'];
+    }
+
+    function disconnect()
+    {
+        if ($this->connected)
+            mssql_close($this->db_handle);
     }
 
     /**
@@ -26,46 +50,26 @@ class db_mssql extends db_base
      */
     function connect()
     {
-        parent::measure_time();
-
-        //MSSQL defaults
-        if (!$this->host)     $this->host     = 'localhost';
-        if (!$this->port)     $this->port     = 1433;
-        if (!$this->username) $this->username = 'root';
-
         //silence warning from failed connection and display our error instead
         $this->db_handle = mssql_pconnect($this->host.':'.$this->port, $this->username, $this->password, true);
 
         if (!$this->db_handle)
-            die('<div class="critical">db_mssql->connect: Conncection Error</div>');
+            die('<div class="critical">db_mssql->connect: Connection Error</div>');
 
-//      if (!$this->db_handle->set_charset($this->charset))
-//          die('Error loading character set '.$this->charset.': '.$this->db_handle->error);
-
-        $this->connected = true;
-        $this->driver    = 'mssql';
-        $this->dialect   = 'mssql';
-//      $this->server_version = $this->db_handle->server_info;
-//      $this->client_version = $this->db_handle->client_info;
+//        if (!$this->db_handle->set_charset($this->charset))
+//            die('Error loading character set '.$this->charset.': '.$this->db_handle->error);
 
         if (!$this->selectDatabase($this->database)) {
             die('Error selecting database');
         }
 
-        parent::measure_connect();
+        $this->connected = true;
     }
 
-    /**
-     * Shows MSSQL driver status
-     */
-    function status()
+    function selectDatabase($dbname)
     {
-        echo 'NO INFO';
-/*      echo 'Host info: '.$this->db_handle->host_info.'<br/>';
-        echo 'Connection character set: '.$this->db_handle->character_set_name().'<br/>';
-        echo 'Last error: '.$this->db_handle->error.'<br/>';
-        echo 'Last errno: '.$this->db_handle->errno;
-*/
+        $this->database = $dbname;
+        return mssql_select_db($this->database, $this->db_handle);
     }
 
     /**
@@ -87,43 +91,22 @@ class db_mssql extends db_base
      * @param $q the query to execute
      * @return result
      */
-    function real_query($q)
+    private function real_query($q)
     {
-        if (!$this->connected) $this->connect();
+        if (!$this->connected)
+            $this->connect();
+
         return mssql_query($q, $this->db_handle);
     }
 
     /**
-     * Executes a SQL query
+     * Performs a query that does a INSERT
      *
-     * @param $q the query to execute
-     * @return result
-     */
-    function query($q)
-    {
-        parent::measure_time();
-
-        $result = $this->real_query($q);
-
-        if (!$result) {
-            if ($this->getDebug()) $this->query_error[ $this->queries_cnt ] = mssql_get_last_message();
-        }
-
-        parent::measure_query($q);
-
-        return $result;
-    }
-
-    /**
-     * For SQL INSERT queries
-     *
-     * @param $q the query to execute
-     * @return insert_id
+     * @param $q is the query to execute
+     * @return insert_id (autoincrement primary key of table)
      */
     function insert($q)
     {
-        parent::measure_time();
-
         $result = $this->real_query($q);
 
         $ret_id = 0;
@@ -134,164 +117,86 @@ class db_mssql extends db_base
 
             $query_result = mssql_fetch_object($query_result);
 
-            $ret_id = $query_result->last_insert_id;;
+            $ret_id = $query_result->last_insert_id;
             mssql_free_result($query_result);
-        } else {
-            if ($this->getDebug()) $this->query_error[ $this->queries_cnt ] = mssql_get_last_message();
         }
-
-        parent::measure_query($q);
 
         return $ret_id;
     }
 
     /**
-     * For SQL DELETE queries
+     * Performs a query that does a REPLACE
      *
-     * @param $q the query to execute
-     * @return number of rows affected
+     * @param $q is the query to execute
+     * @return the number of rows affected
+     */
+    function replace($q) { return $this->insert($q); }
+
+    /**
+     * Performs a query that does a DELETE
+     * Example: DELETE FROM t WHERE id=1
+     *
+     * @param $q is the query to execute
+     * @return the number of rows affected
      */
     function delete($q)
     {
-        parent::measure_time();
-
         $result = $this->real_query($q);
 
         $affected_rows = false;
 
         if ($result) {
             $affected_rows = mssql_rows_affected($this->db_handle);
-        } else {
-            if ($this->getDebug()) $this->query_error[ $this->queries_cnt ] = mssql_get_last_message();
         }
-
-        parent::measure_query($q);
-
+        
         return $affected_rows;
     }
 
     /**
-     * For SQL SELECT queries who returns array of data
+     * Performs a query that does a UPDATE
+     * Example: UPDATE t SET n=1
      *
-     * @param $q the query to execute
-     * @return result
+     * @param $q is the query to execute
+     * @return the number of rows affected
      */
-    function getArray($q)
+    function update($q) { return $this->delete($q); }
+
+    /**
+     * Selects one column of one row of data
+     * Example: SELECT a FROM t WHERE id=1 (where id is distinct)
+     *
+     * @param $q is the query to execute
+     * @return one column-result only
+     */
+    function getOneItem($q)
     {
-        parent::measure_time();
+        if (!$result = $this->real_query($q))
+            return false;
 
-        if (!$result = $this->real_query($q)) {
-            if ($this->getDebug()) $this->profileError($q, mssql_get_last_message());
-            return array();
+        if (mssql_rows_affected($this->db_handle) > 1) {
+            echo "ERROR: DB_MSSQL::getOneItem() returned ".mssql_rows_affected($this->db_handle)." rows!\n";
+            if ($this->getDebug()) echo "Query: ".$q."\n";
+            die;
         }
 
-        $data = array();
-
-        while ($row = mssql_fetch_assoc($result)) {
-            $data[] = $row;
-        }
-
+        $data = mssql_fetch_row($result);
         mssql_free_result($result);
 
-        parent::measure_query($q);
-
-        return $data;
+        if (!$data) return false;
+        return $data[0];
     }
 
     /**
-     * For SQL SELECT queries who returns multiple rows with 1 column of data
+     * Selects one row of data
+     * Example: SELECT * FROM t WHERE id=1 (where id is distinct)
      *
-     * @param $q the query to execute
-     * @return result
-     */
-    function get1dArray($q)
-    {
-        parent::measure_time();
-
-        if (!$result = $this->real_query($q)) {
-            if ($this->getDebug()) $this->profileError($q, mssql_get_last_message());
-            return array();
-        }
-
-        $data = array();
-
-        while ($row = mssql_fetch_row($result))
-            $data[] = $row[0];
-
-        mssql_free_result($result);
-
-        parent::measure_query($q);
-
-        return $data;
-    }
-
-    /**
-     * For SQL SELECT queries who returns mapped array of data
-     *
-     * @param $q the query to execute
-     * @return result
-     */
-    function getMappedArray($q)
-    {
-        parent::measure_time();
-
-        if (!$result = $this->real_query($q)) {
-            if ($this->getDebug()) $this->profileError($q, mssql_get_last_message());
-            return array();
-        }
-
-        $data = array();
-
-        while ($row = mssql_fetch_row($result))
-            $data[ $row[0] ] = $row[1];
-
-        mssql_free_result($result);
-
-        parent::measure_query($q);
-
-        return $data;
-    }
-
-    /**
-     * For SQL SELECT queries who returns array of data with numerical index
-     *
-     * @param $q the query to execute
-     * @return result
-     */
-    function getNumArray($q)
-    {
-        parent::measure_time();
-
-        if (!$result = $this->real_query($q)) {
-            if ($this->getDebug()) $this->profileError($q, mssql_get_last_message());
-            return array();
-        }
-
-        $data = array();
-
-        while ($row = mssql_fetch_row($result))
-            $data[] = $row;
-
-        mssql_free_result($result);
-
-        parent::measure_query($q);
-
-        return $data;
-    }
-
-    /**
-     * For SQL SELECT queries who returns one row of data
-     *
-     * @param $q the query to execute
-     * @return result
+     * @param $q is the query to execute
+     * @return one row-result with columns as array indexes
      */
     function getOneRow($q)
     {
-        parent::measure_time();
-
         if (!$result = $this->real_query($q)) {
-            if ($this->getDebug()) $this->profileError($q, mssql_get_last_message());
-            return array();
+            return false;
         }
 
         if (mssql_rows_affected($db->handle) > 1) {
@@ -303,207 +208,99 @@ class db_mssql extends db_base
         $data = mssql_fetch_array($result, MSSQL_ASSOC);
         mssql_free_result($result);
 
-        parent::measure_query($q);
+        return $data;
+    }
+
+    /**
+     * Selects data
+     * Example: SELECT * FROM t
+     *
+     * @param $q is the query to execute
+     * @return an array with the results, with columns as array indexes
+     */
+    function getArray($q)
+    {
+        if (!$result = $this->real_query($q)) {
+            return false;
+        }
+
+        $data = array();
+
+        while ($row = mssql_fetch_assoc($result)) {
+            $data[] = $row;
+        }
+
+        mssql_free_result($result);
 
         return $data;
     }
 
     /**
-     * For SQL SELECT queries who returns one entry of data
-     *
-     * @param $q the query to execute
-     * @return result
+     * Selects data, 1 column result
+     * Example: SELECT val FROM t WHERE id=3
      */
-    function getOneItem($q)
+    function get1dArray($q)
     {
-        parent::measure_time();
-
         if (!$result = $this->real_query($q)) {
-            if ($this->getDebug()) $this->profileError($q, mssql_get_last_message());
-            return '';
+            return false;
         }
 
-        if (mssql_rows_affected($db->handle) > 1) {
-            echo "ERROR: DB_MSSQL::getOneItem() returned ".mssql_rows_affected($this->db_handle)." rows!\n";
-            if ($this->getDebug()) echo "Query: ".$q."\n";
-            die;
-        }
+        $data = array();
 
-        $data = mssql_fetch_row($result);
+        while ($row = mssql_fetch_row($result))
+            $data[] = $row[0];
+
         mssql_free_result($result);
 
-        parent::measure_query($q);
-
-        if (!$data) return false;
-        return $data[0];
+        return $data;
     }
 
     /**
-     * Lock table from reading
+     * Selects data
+     * Example: SHOW VARIABLES LIKE "%cache%"
      *
-     * @param $t table to lock
-     *
-     * FIXME
+     * @param $q is the query to execute
+     * @return an array with the results mapped as key => value
      */
-    function lock($t)
+    function getMappedArray($q)
     {
-        $this->query('LOCK TABLES '.$t.' READ');
-    }
-
-    /**
-     * Unlock tables
-     *
-     * FIXME
-     */
-    function unlock()
-    {
-        $this->query('UNLOCK TABLES');
-    }
-
-    /**
-     * Returns true if a database with this name already exists
-     *
-     * FIXME
-     */
-    function findDatabase($dbname)
-    {
-        $list = $this->getArray('SHOW DATABASES');
-
-        foreach ($list as $row)
-            if ($row['Database'] == $dbname) return true;
-
-        return false;
-    }
-
-    function selectDatabase($dbname)
-    {
-        $this->database = $dbname;
-        return mssql_select_db($this->database, $this->db_handle);
-    }
-
-    /**
-     *
-     * FIXME
-     */
-    function createDatabase($dbname, $charset = 'utf8')
-    {
-        if ($this->findDatabase($dbname)) return false;
-
-        $q = 'CREATE DATABASE '.$dbname.' CHARACTER SET utf8';
-        return $this->query($q);
-    }
-
-    /**
-     * Returns true if a table with this name already exists
-     *
-     * FIXME
-     */
-    function findTable($tblname)
-    {
-        $list = $this->getNumArray('SHOW TABLES FROM '.$this->database);
-
-        foreach ($list as $row)
-            if ($row[0] == $tblname) return true;
-
-        return false;
-    }
-
-    /**
-     *
-     * FIXME
-     */
-    function createTable($tblname, $layout, $charset = 'utf8')
-    {
-        $parsed = $this->parseLayout($layout);
-
-        $q = "CREATE TABLE ".$tblname." (\n";
-        $key_pri = '';
-        foreach ($parsed as $col) {
-            $q .= $col['Field'].' '.$col['Type'];
-            switch ($col['Null']) {
-            case 'NO': $q .= ' NOT NULL'; break;
-            case 'YES': $q .= ' NULL'; break;
-            }
-            $q .= ($col['Default'] ? " default '".$col['Default']."'" : "");
-            $q .= ($col['Extra'] ? ' '.$col['Extra'] : '').",\n";
-
-            if ($col['Key'] == 'PRI') $key_pri = $col['Field'];
+        if (!$result = $this->real_query($q)) {
+            return false;
         }
-        if ($key_pri) {
-            $q .= "PRIMARY KEY (".$key_pri.")\n";
-        }
-        $q .= ") ENGINE=MyISAM DEFAULT CHARSET=".$charset."\n";
-        return $this->query($q);
+
+        $data = array();
+
+        while ($row = mssql_fetch_row($result))
+            $data[ $row[0] ] = $row[1];
+
+        mssql_free_result($result);
+
+
+        return $data;
     }
 
     /**
+     * Selects data
+     * Example: SELECT textRow FROM t
      *
-     * FIXME
+     * @param $q is the query to execute
+     * @return an 1-dimensional array with a numeric index
      */
-    function verifyTable($tblname, $layout, $charset = 'utf8')
+    function getNumArray($q)
     {
-        $list = $this->getArray('DESCRIBE '.$this->database.'.'.$tblname);
-        if (!$list) return false;
-
-        $parsed = $this->parseLayout($layout);
-
-        if ($list == $parsed) return true;
-        return false;
+        die('fixme update getNumArray');
     }
 
     /**
-     *
-     * FIXME
+     * Shows the config view
      */
-    function parseLayout($layout)
+    public function renderConfig()
     {
-        $res = array();
-        $idx = 0;
-        foreach ($layout as $name=>$col) {
-            $res[$idx]['Field'] = $name;
-            $res[$idx]['Type'] = '';
-            $res[$idx]['Null'] = 'YES';
-            $res[$idx]['Key'] = '';
-            $res[$idx]['Default'] = '';
-            $res[$idx]['Extra'] = '';
-            foreach ($col as $prop) {
-                $ex = explode(':',$prop);
-
-                switch ($ex[0]) {
-                case 'key':
-                    $res[$idx]['Key'] = 'PRI';
-                    break;
-
-                case 'extra':
-                    $res[$idx]['Extra'] = $ex[1];
-                    break;
-
-                case 'default':
-                    $res[$idx]['Default'] = $ex[1];
-                    break;
-
-                case 'null':
-                    $res[$idx]['Null'] = $ex[1];
-                    break;
-
-                case 'datetime':
-                case 'text':
-                case 'smallint':
-                case 'tinyint':
-                case 'bigint':
-                    $res[$idx]['Type'] = $ex[0].(!empty($ex[1]) ? '('.$ex[1].')' : '').(!empty($ex[2]) ? ' '.$ex[2] : '');
-                    break;
-
-                default:
-                    echo "createTable: unknown prop ".$ex[0]."\n";
-                }
-            }
-            $idx++;
-        }
-        return $res;
+        die('IMPLEMENT');
+        $view = new ViewModel('views/mysql_config.php');
+        return $view->render();
     }
 
 }
-
 
 ?>
