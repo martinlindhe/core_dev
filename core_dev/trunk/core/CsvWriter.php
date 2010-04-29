51e7a744a7e9edd1ac0bc22d3072213dd49163fb
Martin Lindhe <martin@ubique.se>
2010-04-29 07:46:43 +0000

    CswWriter fixed to work on default with both Microsoft Office & Openoffice

--- core_dev/trunk/core/CsvWriter.php
+++ core_dev/trunk/core/CsvWriter.php
@@ -4,35 +4,66 @@
  *
  * CSV writer class
  *
- * @author Martin Lindhe, 2010 <martin@startwars.org>
+ * Output using separator ";" and eol "\r\n" is tested & works with
+ * OpenOffice 3.1, Gnumeric 1.9.9 and Microsoft Excel xxx
+ *
+ * http://en.wikipedia.org/wiki/Comma-separated_values
+ *
+ * @author Martin Lindhe, 2008-2010 <martin@startwars.org>
  */
-
-require_once('csv_misc.php');
 
 class CsvWriter
 {
-    private $rows = array();
+    private $rows         = array();
+    private $separator    = ';';
+    private $eol          = "\r\n";
+    private $write_header = true;
 
-    function setData($data)
+    function setData($data) { $this->rows = $data; }
+
+    /**
+     * Escapes data if nessecary
+     */
+    function escape($s)
     {
-        $this->rows = $data;
+        //Fields with embedded double-quote characters must be delimited with double-quote characters,
+        // and the embedded double-quote characters must be represented by a pair of double-quote characters.
+        if (strpos($s, '"') !== false) {
+            return '"'.str_replace('"', '""', $s).'"';
+        }
+
+        //Fields with embedded commas or line breaks must be delimited with double-quote characters.
+        //Fields with leading or trailing spaces must be delimited by double-quote characters.
+        if (
+            strpos($s, $this->separator) !== false ||
+            strpos($s, "\r") !== false || strpos($s, "\n") !== false ||
+            substr($s, 0, 1)  == ' '   || substr($s, -1)    == ' ' ||
+            substr($s, 0, 1)  == "\t"  || substr($s, -1)    == "\t")
+        {
+            return '"'.$s.'"';
+        }
+        return $s;
     }
 
     function render()
     {
-        $header = array();
-        foreach ($this->rows[0] as $idx => $val)
-            $header[] = $idx;
+        $res = '';
 
-        //first row describes the column names
-        $res = '; '.implode(', ', $header)."\r\n";
+        //first row contains the column names
+        if ($this->write_header) {
+            $header = array();
+            foreach ($this->rows[0] as $idx => $val)
+                $header[] = $this->escape($idx);
+
+            $res .= implode($this->separator, $header).$this->eol;
+        }
 
         foreach ($this->rows as $row) {
             $line = array();
             foreach ($row as $col)
-                $line[] = csv_escape($col);
+                $line[] = $this->escape($col);
 
-            $res .= implode(',', $line)."\r\n";
+            $res .= implode($this->separator, $line).$this->eol;
         }
 
         return $res;
