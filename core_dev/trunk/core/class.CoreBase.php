3344680dc4da97a13111b7090e5f1bfa32568672
Martin Lindhe <martin@ubique.se>
2010-06-08 14:11:34 +0000

    re-added __set() override to catch setting of undefined class variables

--- core_dev/trunk/core/class.CoreBase.php
+++ core_dev/trunk/core/class.CoreBase.php
@@ -13,6 +13,14 @@ class CoreBase
 
     function getDebug() { return $this->debug; }
     function setDebug($bool = true) { $this->debug = $bool; }
+
+    /** __set() is run when writing data to inaccessible properties. */
+    public function __set($name, $value)
+    {
+        if (!isset($this->$name))
+            throw new Exception ($name." property does not exist");
+    }
+
 }
 
 ?>
