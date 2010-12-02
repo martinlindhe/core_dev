392b53173c9614bac97913af4c79fdc2fb077fa2
Martin Lindhe <martin@ubique.se>
2010-12-02 12:01:54 +0000

    another attemtp to fix php 5.2 sql profiler

--- core_dev/trunk/core/sql_mysql_profiler.php
+++ core_dev/trunk/core/sql_mysql_profiler.php
@@ -174,8 +174,10 @@ class DatabaseMysqlProfiler extends DatabaseMySQL implements IDB_SQL
 
         $this->measureStart();
 
-        //$res = call_user_func_array(array('parent', 'pSelect'), $args);  // HACK to pass dynamic variables to parent method
-        $res = call_user_func_array('parent::pSelect', $args);  // HACK to pass dynamic variables to parent method
+        if (php_min_ver('5.3'))
+            $res = call_user_func_array(array($this, 'parent::pSelect'), $args);
+        else
+            $res = call_user_func_array(array('parent', 'pSelect'), $args);  // HACK to pass dynamic variables to parent method
 
         $prof = &$this->measureQuery($args[0]);
         $prof->prepared = true;
@@ -202,7 +204,10 @@ class DatabaseMysqlProfiler extends DatabaseMySQL implements IDB_SQL
 
         $this->measureStart();
 
-        $res = call_user_func_array('parent::pUpdate', $args);  // HACK to pass dynamic variables to parent method
+        if (php_min_ver('5.3'))
+            $res = call_user_func_array(array($this, 'parent::pUpdate'), $args);
+        else
+            $res = call_user_func_array(array('parent', 'pUpdate'), $args);  // HACK to pass dynamic variables to parent method
 
         $prof = &$this->measureQuery($args[0]);
         $prof->prepared = true;
