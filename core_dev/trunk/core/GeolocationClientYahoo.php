070fce7ed6185846f04aaeea140e68080b16ec1e
Martin Lindhe <martin@ubique.se>
2010-12-15 21:02:29 +0000

    GeolocationClientYahoo: * parse and return location coordinates

--- core_dev/trunk/core/GeolocationClientYahoo.php
+++ core_dev/trunk/core/GeolocationClientYahoo.php
@@ -14,28 +14,32 @@
 //TODO: update to parse all data such as location
 
 require_once('JSON.php');
+require_once('Coordinate.php');
 
 class GeolocationClientYahooResult
 {
     var $woeid; ///< Yahoo woeid for location
+    var $area;  ///< holds multiple coordinates
 }
 
 class GeolocationClientYahoo
 {
-    private $reader; /// XMLReader
-    private $items = array();
-
     function get($city, $country)
     {
         $q = urlencode('select * from geo.places where text="'.$city.','.$country.'"');
         $url = 'http://query.yahooapis.com/v1/public/yql?q='.$q.'&format=json';
 
         $x = JSON::decode($url);
+        $item = $x->query->results->place[0];
 
         $res = new GeolocationClientYahooResult();
-        $res->woeid = $x->query->results->place[0]->woeid;
-        return $res;
+        $res->woeid = $item->woeid;
+        $res->area = new StdClass();
+        $res->area->center = new Coordinate($item->centroid->latitude, $item->centroid->longitude);
+        $res->area->sw = new Coordinate($item->boundingBox->southWest->latitude, $item->boundingBox->southWest->longitude);
+        $res->area->ne = new Coordinate($item->boundingBox->northEast->latitude, $item->boundingBox->northEast->longitude);
 
+        return $res;
     }
 }
 
