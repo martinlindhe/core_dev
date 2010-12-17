2e0d1fbcfb363b1a35c5ee7794251e269c696613
Martin Lindhe <martin@ubique.se>
2010-12-17 10:37:02 +0000

    output_svg:  *  use just one array with all objects to render, so z-index ordering would work as expected

---
+++ core_dev/trunk/core/ChartLine.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Displays a line chart using SVG graphics
+ *
+ * https://secure.wikimedia.org/wikipedia/en/wiki/Line_chart
+ */
+
+//STATUS: draft, should replace YuiChart (flash)
+
+
+// XXX seee output_svg.php (används bara av fmf/pearl )
+
+//XXX TODO: png render ability
+
+require_once('output_svg.php');
+
+class ChartLine extends SvgImage
+{
+}
+
+?>
--- core_dev/trunk/core/output_svg.php
+++ core_dev/trunk/core/output_svg.php
@@ -13,73 +13,136 @@
  * SVG test suite:
  * http://www.w3.org/Graphics/SVG/Test/
  *
- * @author Martin Lindhe, 2008 <martin@startwars.org>
+ * @author Martin Lindhe, 2008-2010 <martin@startwars.org>
  */
 
-//FIXME: opacity is not quite correct
-//FIXME: use just one array with all objects to render, so z-index ordering would work as expected
+//STATUS: wip
 
-class svg
+//TODO: rename file to SvgImage.php
+
+//FIXME: opacity is not quite correct
+
+class SvgLine
 {
-    var $polygons = array();
-    var $circles = array();
+    var $x1,$x2,$y1,$y2;
+    var $rgb;   //XXXX TODO use a SvgColor class
+    var $stroke_width = 2;
+
+    function render()
+    {
+        //XXX handle RGB
+        $res = '<line x1="'.$this->x1.'" y1="'.$this->y1.'" x2="'.$this->x2.'" y2="'.$this->y2.'" style="stroke:rgb(99,99,99);stroke-width:'.$this->stroke_width.'"/>';
+        return $res;
+    }
+}
+
+
+/**
+ * each array element contains:
+ * ['coords'] a set of X,Y coordinates
+ * ['color'] fill color RGBA
+ * ['border'] border color RGBA
+ */
+class SvgPolygon  //XXX NOT WORKING
+{
+    function render()
+    {
+        $fill_a = ($poly['color'] >> 24) & 0xFF;
+        $fill_a = round($fill_a/127, 2);        //XXX loss of precision
+        if (!$fill_a) $fill_a = 1;    //set missing alpha as 100% alpha
+        $poly['color'] = $poly['color'] & 0xFFFFFF;
+
+        if ($poly['border'] !== false) {
+            $stroke_a = ($poly['border'] >> 24) & 0xFF;
+            $stroke_a = round($stroke_a/127, 2);
+            if (!$stroke_a) $stroke_a = 1;
+            $poly['border'] = $poly['border'] & 0xFFFFFF;
+        }
+
+        $res .=
+        '<polygon'.
+            ' fill="#'.sprintf('%06x', $poly['color']).'"'.
+            ($fill_a < 1 ? ' fill-opacity="'.$fill_a.'"' : '');
+            if ($poly['border'] !== false) {
+                $res .=
+                ' stroke-width="1" stroke="#'.sprintf('%06x', $poly['border']).'"'.
+                ($stroke_a < 1 ? ' stroke-opacity="'.$stroke_a.'"': '');
+            }
+
+        $res .= ' points="';
+        for ($i=0; $i<count($poly['coords']); $i+=2) {
+            $res .= $poly['coords'][$i].','.$poly['coords'][$i+1];
+            if ($i < count($poly['coords'])-2) $res .= ',';
+        }
+        $res .= '"/>';
+
+        return $res;
+    }
+}
+
+
+/**
+ * each array element contains:
+ * ['x'] x-axis coordinate of the center of the circle
+ * ['y'] y-axis coordinate of the center of the circle
+ * ['r'] the radius of the circle
+ * ['color'] fill color RGBA
+ * ['border'] border color RGBA
+ */
+class SvgCircle ///XXX NOT WORKING
+{
+    function render()
+    {
+        $fill_a = ($circ['color'] >> 24) & 0xFF;
+        $fill_a = round($fill_a/127, 2);        //XXX loss of precision
+        if (!$fill_a) $fill_a = 1;    //set missing alpha as 100% alpha
+        $circ['color'] = $circ['color'] & 0xFFFFFF;
+
+        if ($circ['border'] !== false) {
+            $stroke_a = ($circ['border'] >> 24) & 0xFF;
+            $stroke_a = round($stroke_a/127, 2);
+            if (!$stroke_a) $stroke_a = 1;
+            $circ['border'] = $circ['border'] & 0xFFFFFF;
+        }
+
+        $res .=
+        '<circle'.
+            ' fill="#'.sprintf('%06x', $circ['color']).'"'.
+            ($fill_a < 1 ? ' fill-opacity="'.$fill_a.'"' : '');
+            if ($circ['border'] !== false) {
+                $res .=
+                ' stroke-width="1" stroke="#'.sprintf('%06x', $circ['border']).'"'.
+                ($stroke_a < 1 ? ' stroke-opacity="'.$stroke_a.'"': '');
+            }
+
+        $res .= ' cx="'.$circ['x'].'" cy="'.$circ['y'].'" r="'.$circ['r'].'"/>';
+
+        return $res;
+    }
+}
+
+class SvgImage
+{
+    var $objs = array();
 
     var $width, $height;
-    var $bgcolor = false;    ///< background color
+    var $bgcolor = false;    ///< background color, XXX REWORK TO USE SvgColor
 
     function __construct($width = 100, $height = 100)
     {
-        $this->width = $width;
+        $this->width  = $width;
         $this->height = $height;
     }
 
-    /**
-     * each array element contains:
-     * ['coords'] a set of X,Y coordinates
-     * ['color'] fill color RGBA
-     * ['border'] border color RGBA
-     */
-    function addPoly($poly)
+    function add($o)
     {
-        if (!isset($poly['border'])) $poly['border'] = false;
-        $this->polygons[] = $poly;
+        if (!($o instanceof SvgLine))
+            throw new Exception ('only want SvgLine');
+
+        $this->objs[] = $o;
     }
 
-    function addPolys($list)
-    {
-        if (!is_array($list)) return false;
-
-        foreach ($list as $poly) {
-            $this->addPoly($poly);
-        }
-    }
-
-    /**
-     * each array element contains:
-     * ['x'] x-axis coordinate of the center of the circle
-     * ['y'] y-axis coordinate of the center of the circle
-     * ['r'] the radius of the circle
-     * ['color'] fill color RGBA
-     * ['border'] border color RGBA
-     */
-    function addCircle($circ) {
-        if (!isset($circ['border'])) $circ['border'] = false;
-        $this->circles[] = $circ;
-    }
-
-    function addCircles($list)
-    {
-        if (!is_array($list)) return false;
-
-        foreach ($list as $circ) {
-            $this->addCircle($circ);
-        }
-    }
-
-    function setBackground($col)
-    {
-        $this->bgcolor = $col;
-    }
+    function setBackground($col) { $this->bgcolor = $col; }
 
     function render()
     {
@@ -95,62 +158,8 @@ class svg
             '<rect x="0" y="0" width="'.$this->width.'" height="'.$this->height.'" fill="#'.sprintf('%06x', $this->bgcolor).'"/>';
         }
 
-        foreach ($this->polygons as $poly) {
-            $fill_a = ($poly['color'] >> 24) & 0xFF;
-            $fill_a = round($fill_a/127, 2);        //XXX loss of precision
-            if (!$fill_a) $fill_a = 1;    //set missing alpha as 100% alpha
-            $poly['color'] = $poly['color'] & 0xFFFFFF;
-
-            if ($poly['border'] !== false) {
-                $stroke_a = ($poly['border'] >> 24) & 0xFF;
-                $stroke_a = round($stroke_a/127, 2);
-                if (!$stroke_a) $stroke_a = 1;
-                $poly['border'] = $poly['border'] & 0xFFFFFF;
-            }
-
-            $res .=
-            '<polygon'.
-                ' fill="#'.sprintf('%06x', $poly['color']).'"'.
-                ($fill_a < 1 ? ' fill-opacity="'.$fill_a.'"' : '');
-                if ($poly['border'] !== false) {
-                    $res .=
-                    ' stroke-width="1" stroke="#'.sprintf('%06x', $poly['border']).'"'.
-                    ($stroke_a < 1 ? ' stroke-opacity="'.$stroke_a.'"': '');
-                }
-
-            $res .= ' points="';
-            for ($i=0; $i<count($poly['coords']); $i+=2) {
-                $res .= $poly['coords'][$i].','.$poly['coords'][$i+1];
-                if ($i < count($poly['coords'])-2) $res .= ',';
-            }
-            $res .=
-            '"/>';
-        }
-
-        foreach ($this->circles as $circ) {
-            $fill_a = ($circ['color'] >> 24) & 0xFF;
-            $fill_a = round($fill_a/127, 2);        //XXX loss of precision
-            if (!$fill_a) $fill_a = 1;    //set missing alpha as 100% alpha
-            $circ['color'] = $circ['color'] & 0xFFFFFF;
-
-            if ($circ['border'] !== false) {
-                $stroke_a = ($circ['border'] >> 24) & 0xFF;
-                $stroke_a = round($stroke_a/127, 2);
-                if (!$stroke_a) $stroke_a = 1;
-                $circ['border'] = $circ['border'] & 0xFFFFFF;
-            }
-
-            $res .=
-            '<circle'.
-                ' fill="#'.sprintf('%06x', $circ['color']).'"'.
-                ($fill_a < 1 ? ' fill-opacity="'.$fill_a.'"' : '');
-                if ($circ['border'] !== false) {
-                    $res .=
-                    ' stroke-width="1" stroke="#'.sprintf('%06x', $circ['border']).'"'.
-                    ($stroke_a < 1 ? ' stroke-opacity="'.$stroke_a.'"': '');
-                }
-
-            $res .= ' cx="'.$circ['x'].'" cy="'.$circ['y'].'" r="'.$circ['r'].'"/>';
+        foreach ($this->objs as $o) {
+            $res .= $o->render();
         }
 
         $res .=
