4286db4d5fda9b8ac74e5ccadda0aba10ffe8c8e
Martin Lindhe <martin@ubique.se>
2011-02-04 20:15:48 +0000

    FIX: made coordinates to constructor optional NEW: addMarkers() adds an array of map markers to the map

--- core_dev/trunk/core/GoogleMapsJs.php
+++ core_dev/trunk/core/GoogleMapsJs.php
@@ -9,6 +9,8 @@
 
 //STATUS: wip
 
+//FIXME: add title text to map markers
+
 //TODO: make mapTypeId configurable: ROADMAP, SATELLITE, HYBRID, TERRAIN
 //TODO: docs mentioned a version param to force a exact version of the api for production sites, use this to match a working implementation!
 
@@ -22,18 +24,28 @@ class GoogleMapsJs
     protected $region;                  ///< ("US", or "SE") in order to force maps to assume it is viewed from this region, rather than detected region
 
     protected $detect_location = false; ///< shall google maps try to detect location of the user? .. XXX WHY??? whats the benefit of setting this?
-    protected $zoom   = 1;              ///< 1 (whole world) to 20 (max zoom)
+    protected $zoom = 1;                ///< 1 (whole world) to 20 (max zoom)
 
     protected $width  = 600;
     protected $height = 400;
+    protected $markers = array();
 
-    function __construct($lat, $lng)
+    function __construct($lat = 0, $lng = 0)
     {
         $this->latitude  = $lat;
         $this->longitude = $lng;
     }
 
     function setZoomLevel($n) { $this->zoom = $n; }
+
+    function addMarkers($arr)
+    {
+        if (!is_array($arr))
+            throw new Exception ('not array');
+
+        foreach ($arr as $o)
+            $this->markers[] = $o;
+    }
 
     function render()
     {
@@ -48,13 +60,30 @@ class GoogleMapsJs
         $div_id = 'gm_map_'.mt_rand();
 
         $res =
-        'var ll = new google.maps.LatLng('.$this->latitude.', '.$this->longitude.');'.
+        'var ll = new google.maps.LatLng('.$this->latitude.','.$this->longitude.');'.
         'var myOptions = {'.
             'center: ll,'.
             'zoom: '.$this->zoom.','.
             'mapTypeId: google.maps.MapTypeId.ROADMAP'.
         '};'.
-        'var map = new google.maps.Map(document.getElementById("'.$div_id.'"), myOptions);';
+        'var myMap = new google.maps.Map(document.getElementById("'.$div_id.'"), myOptions);';
+
+        if ($this->markers) {
+            $res .= 'var locationArray = [';
+            foreach ($this->markers as $m)
+                $res .= 'new google.maps.LatLng('.$m[0].','.$m[1].'),';
+            $res .= '];';
+
+            $res .=
+            'var coord;'.
+            'for (coord in locationArray) {'.
+                'new google.maps.Marker({'.
+                    'position: locationArray[coord],'.
+                    'map: myMap,'.
+    //                'title: locationNameArray[coord]'.
+                '});'.
+            '}';
+        }
 
         return
         '<div id="'.$div_id.'" style="width:'.$this->width.'px; height:'.$this->height.'px"/>'.
