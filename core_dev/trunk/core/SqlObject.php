b953b9f8fc3c6fb78ae7a541de2369b9d4fe0c1c
Martin Lindhe <martin@ubique.se>
2011-02-12 18:03:06 +0000

    * made createObject() also accept an array as input * added sanity checking

--- core_dev/trunk/core/SqlObject.php
+++ core_dev/trunk/core/SqlObject.php
@@ -6,22 +6,28 @@
 class SqlObject
 {
     /**
-     * @param $q       a sql select query resulting in multiple rows
+     * @param $q       a sql select query resulting in multiple rows, or a array of rows
      * @param $objname name of class object to load rows into
      */
     static function createObject($q, $objname)
     {
         $db = SqlHandler::getInstance();
 
+        $list = is_array($q) ? $q : $db->pSelect($q);
+
         $reflect = new ReflectionClass($objname);
         $props   = $reflect->getProperties(ReflectionProperty::IS_PUBLIC | ReflectionProperty::IS_PROTECTED);
 
         $res = array();
-        foreach ($db->getArray($q) as $row)
+        foreach ($list as $row)
         {
             $obj = new $objname();
             foreach ($props as $prop) {
                 $n = $prop->getName();
+                if (!array_key_exists($n, $row)) {
+                    d( $row);
+                    throw new Exception ('SqlObject not right! db column named "'.$n.'" dont exist');
+                }
                 $obj->$n = $row[ $n ];
             }
 
