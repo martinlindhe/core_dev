7b78157aa0fcd0ab2b524a415ba73bf0c010c8cd
Martin Lindhe <martin@ubique.se>
2011-02-12 17:18:46 +0000

    helper class added to load a row into a object, like:
    class SomeObj
    {
        var $name;
        var $desc;
    }
    
    $q = 'SELECT * FROM tblObjects ORDER BY name ASC';
    $objs = SqlToObject::create($q, 'SomeObj');

--- core_dev/trunk/core/sql_misc.php
+++ core_dev/trunk/core/sql_misc.php
@@ -7,6 +7,38 @@
  * @author Martin Lindhe, 2009-2011 <martin@startwars.org>
  */
 
+/** reads all db columns into properties of a object */
+class SqlToObject
+{
+    /**
+     * @param $q       a sql select query resulting in multiple rows
+     * @param $objname name of class object to load rows into
+     */
+    static function create($q, $objname)
+    {
+        $db = SqlHandler::getInstance();
+
+        $obj = new $objname();
+
+        $reflect = new ReflectionClass($obj);
+        $props   = $reflect->getProperties(ReflectionProperty::IS_PUBLIC | ReflectionProperty::IS_PROTECTED);
+
+        $res = array();
+        foreach ($db->getArray($q) as $row)
+        {
+            foreach ($props as $prop) {
+                $n = $prop->getName();
+                $obj->$n = $row[ $n ];
+            }
+
+            $res[] = $obj;
+        }
+
+        return $res;
+    }
+}
+
+
 /**
  * @param $ts UNIX timestamp
  * @return given UNIX timestamp in MySQL datetime format (YYYY-MM-DD HH:MM:SS)
