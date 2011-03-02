bff148855451fb27d6fe9c776c83b1adb4a4664c
Martin Lindhe <martin@ubique.se>
2011-03-02 16:32:19 +0000

    byte_to_bits() added

--- core_dev/trunk/core/bits.php
+++ core_dev/trunk/core/bits.php
@@ -21,4 +21,23 @@ function byte_split($b)
         );
 }
 
+/** Returns array with bit values of input byte */
+function byte_to_bits($b)
+{
+    if (is_string($b))
+        $b = ord($b);
+
+    if (!is_numeric($b) || $b > 255 || $b < 0)
+        throw new Exception ('bad input: '.$b);
+
+    $res = array();
+
+    $bits = array(1, 2, 4, 8, 16, 32, 64, 128);
+
+    for ($i = 7; $i >= 0; $i--)
+        $res[$i] = ($b & $bits[$i]) ? 1 : 0;
+
+    return $res;
+}
+
 ?>
