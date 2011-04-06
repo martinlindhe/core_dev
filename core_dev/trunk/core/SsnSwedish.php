a9c406f66bb4930931f7e71eb99e821c8bf1d403
Martin Lindhe <martin@ubique.se>
2011-04-06 09:44:12 +0000

    added getTimestamp() and getGender() methods to SsnSwedish + more cleanup

--- core_dev/trunk/core/SsnSwedish.php
+++ core_dev/trunk/core/SsnSwedish.php
@@ -38,36 +38,63 @@ class SsnSwedish
 
     protected $valid = false;
 
-    static function isValid($ssn, $gender = 0)
+    /* @return 12-digit ssn YYYYMMDDXXXX */
+    private static function cleanSsn($ssn)
     {
         $ssn = str_replace('-', '', $ssn);
         $ssn = str_replace(' ', '', $ssn);
+        $ssn = trim($ssn);
 
-        //"YYYY" is converted to "YY"
-        if (strlen($ssn) == 12)
-            $ssn = substr($ssn, 2);
+        switch (strlen($ssn)) {
+        case 10:
+            // "YY" is converted to "YYYY"
+            // years below current year is considered to be 2000-20xx, otherwise its 1900-19xx
+            $yr = substr($ssn, 0, 2);
+            $yr = ($yr > date('y')) ? '19'.$yr : '20'.$yr;
 
-        if (strlen($ssn) != 10 || !is_numeric($ssn))
-            throw new Exception ('SsnSwedish set to invalid ssn: '.$ssn);
+            $mn = substr($ssn, 2, 2);
+            $dy = substr($ssn, 4, 2);
+            $last4 = substr($ssn, 6, 4);
+            break;
+        case 12:
+            $yr = substr($ssn, 0, 4);
+            // years in the future cant be valid ssn
+            if ($yr > date('Y'))
+                return false;
+            $mn = substr($ssn, 4, 2);
+            $dy = substr($ssn, 6, 2);
+            $last4 = substr($ssn, 8, 4);
+            break;
 
-        //years below current year is considered to be 2000-20xx, otherwise its 1900-19xx
-        $yr = substr($ssn, 0, 2);
-        $yr = ($yr > date('y')) ? '19'.$yr : '20'.$yr;
+        default:
+            return false;
+        }
 
-        $mn = substr($ssn, 2, 2);
-        $dy = substr($ssn, 4, 2);
-
-        //validate if the date existed, for example 19810230 is invalid
+        // validate if the date existed, for example 19810230 is invalid
         if (!checkdate($mn, $dy, $yr))
             return false;
 
-        $ssn_gender = substr($ssn, 8, 1);
+        return $yr.$mn.$dy.$last4;
+    }
 
-        //Error: odd (male) ssn found but user thinks its a female ssn
+    /**
+     * @param $ssn a swedish SSN in the format "YYYYMMDD-XXXX" or "YYMMDD-XXXX"
+     * @return true if checksum is correct
+     */
+    static function isValid($ssn, $gender = 0)
+    {
+        $ssn = self::cleanSsn($ssn);
+
+        if (!$ssn)
+            return false;
+
+        $ssn_gender = substr($ssn, 10, 1);
+
+        // Error: odd (male) ssn found but user thinks its a female ssn
         if ($gender && ($ssn_gender % 2) && $gender == SsnSwedish::FEMALE)
             throw new Exception ('Wrong gender specified, this ssn belongs to a male');
 
-        //Error: even (female) ssn found but user thinks its a male ssn
+        // Error: even (female) ssn found but user thinks its a male ssn
         if ($gender && !($ssn_gender % 2) && $gender == SsnSwedish::MALE)
             throw new Exception ('Wrong gender specified, this ssn belongs to a female');
 
@@ -81,25 +108,56 @@ class SsnSwedish
      * Calculates the checksum of a swedish social security number (personnummer)
      *
      * Using the Luhn algorithm
-     *
-     * @param $ssn a swedish SSN in the format "YYYYMMDD-XXXX" or "YYMMDD-XXXX"
-     * @return true if checksum is correct
      */
     private static function calcLunh($ssn, $gender = 0)
     {
+        if (strlen($ssn) != 12)
+            throw new Exception ('XXX should not happen');
+
+        $ssn = substr($ssn, 2); // remove first 2 digits of YEAR
+
         $sum = 0;
 
         for ($i = 0; $i < strlen($ssn)-1; $i++)
         {
-            $tmp = substr($ssn, $i, 1) * (2 - ($i & 1)); //Switch between 212121212
+            $tmp = substr($ssn, $i, 1) * (2 - ($i & 1)); // Switch between 212121212
             if ($tmp > 9) $tmp -= 9;
             $sum += $tmp;
         }
 
-        //Substract the ones place digit from 10
+        // Substract the ones place digit from 10
         $sum = (10 - ($sum % 10)) % 10;
 
         return substr($ssn, -1, 1) == $sum;
+    }
+
+    static function getTimestamp($ssn)
+    {
+        $ssn = self::cleanSsn($ssn);
+
+        if (strlen($ssn) != 12 || !is_numeric($ssn))
+            throw new Exception ('isValid invalid ssn: '.$ssn);
+
+        return strtotime($ssn);
+    }
+
+    static function getGender($ssn)
+    {
+        $ssn = self::cleanSsn($ssn);
+
+        if (!$ssn)
+            return false;
+
+        if (!self::calcLunh($ssn))
+            return false;
+
+        $gender = substr($ssn, 10, 1);
+
+        if ($gender % 2)
+            return 'M';
+
+        if (!$gender % 2)
+            return 'F';
     }
 
     static function render($ssn)
--- core_dev/trunk/tests/test.SsnSwedish.php
+++ core_dev/trunk/tests/test.SsnSwedish.php
@@ -3,21 +3,26 @@
 set_include_path(get_include_path() . PATH_SEPARATOR . dirname(__FILE__).'/../core/');
 
 require_once('SsnSwedish.php');
+require_once('core.php');
 
 if (!SsnSwedish::isValid('811218-9876')) echo "FAIL 1\n";  //known correct
 if (!SsnSwedish::isValid('800726-2010')) echo "FAIL 2\n";  //known correct
 if (SsnSwedish::isValid('800726-2222'))  echo "FAIL 3\n";  //known fake
 if (!SsnSwedish::isValid('800222-0138')) echo "FAIL 4\n";  //known correct
 
-if (!SsnSwedish::isValid('800726-2010', SsnSwedish::MALE)) echo "FAIL 5\n"; // known to be a male ssn
+if (!SsnSwedish::isValid('19800726-2010')) echo "FAIL 5\n";  //known correct
+if (SsnSwedish::isValid('20800726-2010')) echo "FAIL 6\n";  // BAD, in the future!
 
+if (!SsnSwedish::isValid('800726-2010', SsnSwedish::MALE)) echo "FAIL 7\n"; // known to be a male ssn
 
-if (!OrgNoSwedish::isValid('556684-8635')) echo "FAIL 6\n"; //Kryptonit AB
-if (!OrgNoSwedish::isValid('556455-4656')) echo "FAIL 7\n"; //Unicorn Communications AB
-if (!OrgNoSwedish::isValid('556632-0221')) echo "FAIL 8\n"; //Unicorn Interactive AB
-if (!OrgNoSwedish::isValid('556539-1355')) echo "FAIL 9\n"; //Unicorn Telecom AB
+if (sql_date( SsnSwedish::getTimestamp('800726-2010') ) != '1980-07-26') echo "FAIL8\n";
 
+if (SsnSwedish::getGender('800726-2010') != 'M') echo "FAIL 9\n";
 
-echo SsnSwedish::getTimestamp('800726-2222');
+if (!OrgNoSwedish::isValid('556684-8635')) echo "FAIL 10\n"; //Kryptonit AB
+if (!OrgNoSwedish::isValid('556455-4656')) echo "FAIL 11\n"; //Unicorn Communications AB
+if (!OrgNoSwedish::isValid('556632-0221')) echo "FAIL 12\n"; //Unicorn Interactive AB
+if (!OrgNoSwedish::isValid('556539-1355')) echo "FAIL 13\n"; //Unicorn Telecom AB
+
 
 ?>
