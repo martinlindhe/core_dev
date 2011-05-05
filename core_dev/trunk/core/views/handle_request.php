2567899a11a4ca8a19e0ddac30a4867adf44ff2e
Martin Lindhe <martin@ubique.se>
2011-05-05 12:48:57 +0000

    sync

--- core_dev/trunk/core/views/handle_request.php
+++ core_dev/trunk/core/views/handle_request.php
@@ -4,8 +4,9 @@ require_once('RegisterHandler.php');
 
 $session->resume();
 
-//Logged in: Check if client ip has changed since last request, if so - log user out to avoid session hijacking
-if ($session->id && $session->ip && ($session->ip != client_ip()) ) {
+if ($session->id && $session->ip && ($session->ip != client_ip()) )
+{
+    // Logged in: Check if client ip has changed since last request, if so - log user out to avoid session hijacking
     $msg = 'ERROR: Client IP changed! Old: '.$session->ip.', current: '.client_ip();
     $error->add($msg);
 //die($msg);
@@ -13,15 +14,14 @@ if ($session->id && $session->ip && ($session->ip != client_ip()) ) {
     $session->end();
 //    $session->errorPage();
 }
-
-//Check for login request, POST to any page with 'login_usr' & 'login_pwd' variables set to log in
-if (!$session->id && !empty($_POST['login_usr']) && isset($_POST['login_pwd']))
+else if (!$session->id && !empty($_POST['login_usr']) && isset($_POST['login_pwd']))
 {
+    // Check for login request, POST to any page with 'login_usr' & 'login_pwd' variables set to log in
     $session->login($_POST['login_usr'], $_POST['login_pwd']);
 }
-//Logged in: Check for a logout request. Send GET parameter 'logout' to any page to log out
 else if ($session->id && isset($_GET['logout']))
 {
+    // Logged in: Check for a logout request. Send GET parameter 'logout' to any page to log out
     $session->logout();
 }
 
