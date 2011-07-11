4c22c3947e18ef3ea97fd914603080be729a7e08
Martin Lindhe <martin@ubique.se>
2011-07-11 15:24:08 +0000

    added more testcases

--- core_dev/trunk/tests/test.time.php
+++ core_dev/trunk/tests/test.time.php
@@ -15,17 +15,18 @@ if (elapsed_seconds(60 * 60 * 24 * 365 * 2) != '2 years') echo "FAIL 7\n";
 if (in_seconds('00:00:00') != 0)                          echo "FAIL 8\n";
 if (in_seconds('18:13:45') != 65625)                      echo "FAIL 9\n";
 if (in_seconds('23:59:59') != 86399)                      echo "FAIL 10\n";
+if (in_seconds('0:49:47.53') != 2987.53)                  echo "FAIL 11\n";
 
-if (parse_duration('4h') != 14400)                        echo "FAIL 11\n";
+if (parse_duration('4h') != 14400)                        echo "FAIL 12\n";
 
-if (!is_duration('2d'))                                   echo "FAIL 12\n";
-if (is_duration('abc'))                                   echo "FAIL 13\n";
-if (is_duration('1a2d'))                                  echo "FAIL 13\n";
-if (!is_duration( 500 ))                                  echo "FAIL 14\n";
+if (!is_duration('2d'))                                   echo "FAIL 13\n";
+if (is_duration('abc'))                                   echo "FAIL 14\n";
+if (is_duration('1a2d'))                                  echo "FAIL 15\n";
+if (!is_duration( 500 ))                                  echo "FAIL 16\n";
 
-if ( sql_date(ts('2011-05-08')) != '2011-05-08')          echo "FAIL 15\n";
-if ( sql_date(ts('20110508')) != '2011-05-08')            echo "FAIL 16\n";
-if ( sql_date(ts('5/8/2011')) != '2011-05-08')            echo "FAIL 17\n";
-if ( sql_date(ts('05/08/2011')) != '2011-05-08')          echo "FAIL 18\n";
+if ( sql_date(ts('2011-05-08')) != '2011-05-08')          echo "FAIL 17\n";
+if ( sql_date(ts('20110508')) != '2011-05-08')            echo "FAIL 18\n";
+if ( sql_date(ts('5/8/2011')) != '2011-05-08')            echo "FAIL 19\n";
+if ( sql_date(ts('05/08/2011')) != '2011-05-08')          echo "FAIL 20\n";
 
 ?>
