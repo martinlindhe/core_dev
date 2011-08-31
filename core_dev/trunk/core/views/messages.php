bc77e0b9e6b61c1beb99670d7dfc7a15036fade9
Martin Lindhe <martin@ubique.se>
2011-08-31 23:58:02 +0000

    sync

--- core_dev/trunk/core/views/messages.php
+++ core_dev/trunk/core/views/messages.php
@@ -3,6 +3,11 @@
  * Default view for User's Private Message Inbox and Outbox
  */
 
+//TODO: ability to view 1 full msg (there also see previous conversation with that person)
+//TODO: mark seen msg as "READ"
+//TODO: ability to mark seen msg as "UNREAD" again
+//TODO: ability to mark a message as "DELETED". really useful? will require 2 copies of each msg, or else if user 1 deletes the msg, user2 also wont see it
+
 require_once('Message.php');
 
 require_once('YuiDatatable.php');
