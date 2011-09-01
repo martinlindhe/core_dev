823a1aa3dfcc59133dcda573b73b642a8b14a041
Martin Lindhe <martin@ubique.se>
2011-09-01 19:45:03 +0000

    * show current user privilegies * use prepared statements

--- core_dev/trunk/core/views/mysql_config.php
+++ core_dev/trunk/core/views/mysql_config.php
@@ -17,14 +17,25 @@ echo 'Username: <b>'.$db->username.'</b><br/>';
 // echo 'Password: '.($db->password ? $db->password : '(blank)').'<br/>';
 echo 'Database: '.$db->database.'<br/>';
 echo 'Configured charset: '.$db->charset.'<br/>';
-echo '<br/>';
+
+
+
+$q = 'SHOW GRANTS FOR CURRENT_USER';
+$priv = Sql::pSelectItem($q);
+//XXXX: parse output and complain if too low or too high priviliegies!
+// example:  GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, ALTER ON *.* TO 'root'@'%' IDENTIFIED BY PASSWORD '*xxx'
+// example2: GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' IDENTIFIED BY PASSWORD '*xxx' WITH GRANT OPTION
+echo 'Privilegies: <b>'.substr($priv, 0, 70).'</b>...<br/>';
+echo'<br/>';
+
+
 
 echo '<h2>Database time</h2>';
-$db_time = $db->getOneItem('SELECT NOW()');
+$db_time = Sql::pSelectItem('SELECT NOW()');
 echo 'Database time: '.$db_time.'<br/>';
 echo 'Webserver time: '.now().'<br/>';
 
-$uptime = $db->getOneRow('SHOW STATUS WHERE Variable_name="Uptime"');
+$uptime = Sql::pSelectRow('SHOW STATUS WHERE Variable_name = ?', 's', 'Uptime');
 echo 'Database uptime: '.elapsed_seconds($uptime['Value']).'<br/>';
 
 echo '<br/>';
@@ -37,8 +48,11 @@ echo 'Last errno: '.$db->db_handle->errno.'<br/>';
 echo '<br/>';
 
 // show MySQL query cache settings
-$data = $db->getMappedArray('SHOW VARIABLES LIKE "%query_cache%"');
-if ($data['have_query_cache'] == 'YES') {
+
+$data = Sql::pSelectMapped('SHOW VARIABLES LIKE "%query_cache%"');
+
+if ($data['have_query_cache'] == 'YES')
+{
     echo '<h2>MySQL query cache settings</h2>';
     echo 'Type: '. $data['query_cache_type'].'<br/>';        //valid values: ON, OFF or DEMAND
     echo 'Size: '. formatDataSize($data['query_cache_size']).' (total size)<br/>';
@@ -47,7 +61,8 @@ if ($data['have_query_cache'] == 'YES') {
     echo 'Wlock invalidate: '. $data['query_cache_wlock_invalidate'].'<br/><br/>';
 
     // current query cache status
-    $data = $db->getMappedArray('SHOW STATUS LIKE "%Qcache%"', 'Variable_name', 'Value');
+    $data = Sql::pSelectMapped('SHOW STATUS LIKE "%Qcache%"');
+
     echo '<h2>MySQL query cache status</h2>';
     echo 'Hits: '. formatNumber($data['Qcache_hits']).'<br/>';
     echo 'Inserts: '. formatNumber($data['Qcache_inserts']).'<br/>';
@@ -59,7 +74,9 @@ if ($data['have_query_cache'] == 'YES') {
     echo '<br/>';
     echo 'Free blocks: '. formatNumber($data['Qcache_free_blocks']).'<br/>';
     echo 'Lowmem prunes: '. formatNumber($data['Qcache_lowmem_prunes']);
-} else {
+}
+else
+{
     echo '<h2>MySQL query cache is disabled</h2>';
 }
 
