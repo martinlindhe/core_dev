677f950d930fd6ad324849851f96491dff897dd0
Martin Lindhe <martin@ubique.se>
2011-09-01 15:03:38 +0000

    NEW: can now write in other peoples guestbooks

--- core_dev/trunk/core/views/profile_guestbook.php
+++ core_dev/trunk/core/views/profile_guestbook.php
@@ -1,6 +1,5 @@
 <?php
 
-//TODO: ability post entries in someone elses guestbook
 //TODO: mark entries as read, see number of unread entries
 
 require_once('Guestbook.php');
@@ -25,14 +24,44 @@ $gb = Guestbook::getEntries($user_id);
 d( $gb);
 
 
+if ($user_id == $session->id)
+    return;
 
-/*
-$x = new Guestbook();
-$x->owner = $session->id;
-$x->creator = 123;
-$x->time_created = sql_datetime( time() );
-$x->body = 'hej svejs din gamle pelikan';
-Guestbook::store($x);
-*/
+$header->registerJsFunction(
+'function check_gb(frm)'.
+'{'.
+    'if (!frm.body.value)'.
+        'return false;'.
+    'return true;'.
+'}'
+);
+
+function gbHandler($p)
+{
+    $session = SessionHandler::getInstance();
+
+    if ($session->id == $p['to'])
+        return false;
+
+    $x = new Guestbook();
+    $x->owner = $p['to'];
+    $x->creator = $session->id;
+    $x->time_created = sql_datetime( time() );
+    $x->body = $p['body'];
+    Guestbook::store($x);
+
+    return true;
+}
+
+echo '<h1>New guestbook entry to '.$user->name.'</h1>';
+
+$form = new XhtmlForm('msg');
+$form->addHidden('to', $this->owner);
+$form->addTextarea('body', 'Body');
+$form->addSubmit('Send');
+$form->setFocus('body');
+$form->onSubmit('return check_gb(this);');
+$form->setHandler('gbHandler');
+echo $form->render();
 
 ?>
