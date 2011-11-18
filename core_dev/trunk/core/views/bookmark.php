4ba0768ab5f6ca649bfb0444dbc6db129c019100
Martin Lindhe <martin@ubique.se>
2011-11-18 14:13:37 +0000

    NEW: added ability to list all favorite users + remove favorite links

--- core_dev/trunk/core/views/bookmark.php
+++ core_dev/trunk/core/views/bookmark.php
@@ -8,13 +8,28 @@ switch ($this->owner) {
 case 'adduser':
     // child = user id
     Bookmark::create(BOOKMARK_FAVORITEUSER, $this->child);
-    js_redirect('iview/profile/'.$this->child);
+    js_redirect('iview/bookmark/listusers');
     break;
 
 case 'removeuser':
     // child = user id
     Bookmark::remove(BOOKMARK_FAVORITEUSER, $this->child);
-    js_redirect('iview/profile/'.$this->child);
+    js_redirect('iview/bookmark/listusers');
+    break;
+
+
+case 'listusers':
+    echo '<h1>Favorite users</h1>';
+
+    $bookmarks = Bookmark::getList(BOOKMARK_FAVORITEUSER, $session->id);
+    foreach ($bookmarks as $bm)
+    {
+        $u = User::get($bm->value);
+        echo ahref('iview/profile/'.$u->id, $u->name);
+        echo ' ';
+        echo ahref('iview/bookmark/removeuser/'.$u->id, 'Remove');
+        echo '<br/>';
+    }
     break;
 
 default:
