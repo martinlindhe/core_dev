dd115345cf58a3dbeff2011df05036596927ac68
Martin Lindhe <martin@ubique.se>
2011-11-23 13:01:43 +0000

    ConvertDatasize: added all big datatypes: petabit/byte, exabit/byte, zettabit/byte, yottabit/byte

--- core_dev/trunk/core/ConvertDatasize.php
+++ core_dev/trunk/core/ConvertDatasize.php
@@ -16,31 +16,51 @@ require_once('ConvertBase.php');
 class ConvertDatasize extends ConvertBase
 {
     protected $scale = array( ///< unit scale to a bit
+    // http://en.wikipedia.org/wiki/Template:Quantities_of_bits
     'bit'  => 1,
-    'kbit' => 1024,       // 2^10
-    'mbit' => 1048576,    // 2^20
-    'gbit' => 1073741824, // 2^30
+    'kbit' => 1024,                      // 2^10
+    'mbit' => 1048576,                   // 2^20
+    'gbit' => 1073741824,                // 2^30
+    'tbit' => 1099511627776,             // 2^40
+    'pbit' => 1125899906842624,          // 2^50
+    'ebit' => 1152921504606846976,       // 2^60
+    'zbit' => 1180591620717411303424,    // 2^70
+    'ybit' => 1208925819614629174706176, // 2^80
 
+    // http://en.wikipedia.org/wiki/Template:Quantities_of_bytes
     'b'    => 8,
-    'kb'   => 8192,            // (2^10)*8
-    'mb'   => 8388608,         // (2^20)*8
-    'gb'   => 8589934592,      // (2^30)*8
-    'tb'   => 8796093022208,   // (2^40)*8
-    'pb'   => 9007199254740992,// (2^50)*8
+    'kb'   => 8192,                      // (2^10)*8
+    'mb'   => 8388608,                   // (2^20)*8
+    'gb'   => 8589934592,                // (2^30)*8
+    'tb'   => 8796093022208,             // (2^40)*8
+    'pb'   => 9007199254740992,          // (2^50)*8
+    'eb'   => 9223372036854775808,       // (2^60)*8
+    'zb'   => 9444732965739290427392,    // (2^70)*8
+    'yb'   => 9671406556917033397649408, // (2^80)*8
     );
 
     protected $lookup = array(
-    'bit'      => 'bit',  'bits'     => 'bit',
-    'kilobit'  => 'kbit', 'kilobits' => 'kbit',
-    'megabit'  => 'mbit', 'megabits' => 'mbit',
-    'gigabit'  => 'gbit', 'gigabits' => 'gbit',
+    // bit counts
+    'bit'      => 'bit',  'bits'      => 'bit',
+    'kilobit'  => 'kbit', 'kilobits'  => 'kbit',
+    'megabit'  => 'mbit', 'megabits'  => 'mbit',
+    'gigabit'  => 'gbit', 'gigabits'  => 'gbit',
+    'terabit'  => 'tbit', 'terabits'  => 'tbit',
+    'petabit'  => 'pbit', 'petabits'  => 'pbit',
+    'exabit'   => 'ebit', 'exabits'   => 'ebit',
+    'zettabit' => 'zbit', 'zettabits' => 'zbit',
+    'yottabit' => 'ybit', 'yottabits' => 'ybit',
 
-    'byte'     => 'b',
-    'kilobyte' => 'kb', 'kbyte' => 'kb', 'kib' => 'kb', 'k' => 'kb',
-    'megabyte' => 'mb', 'mbyte' => 'mb', 'mib' => 'mb', 'm' => 'mb',
-    'gigabyte' => 'gb', 'gbyte' => 'gb', 'gib' => 'gb', 'g' => 'gb',
-    'terabyte' => 'tb', 'tbyte' => 'tb', 'tib' => 'tb', 't' => 'tb',
-    'petabyte' => 'pb',
+    // byte counts
+    'byte'      => 'b',
+    'kilobyte'  => 'kb', 'kbyte' => 'kb', 'kib' => 'kb', 'k' => 'kb',
+    'megabyte'  => 'mb', 'mbyte' => 'mb', 'mib' => 'mb', 'm' => 'mb',
+    'gigabyte'  => 'gb', 'gbyte' => 'gb', 'gib' => 'gb', 'g' => 'gb',
+    'terabyte'  => 'tb', 'tbyte' => 'tb', 'tib' => 'tb', 't' => 'tb',
+    'petabyte'  => 'pb',
+    'exabyte'   => 'eb',
+    'zettabyte' => 'zb',
+    'yottabyte' => 'yb',
     );
 
     /**
--- core_dev/trunk/tests/test.ConvertDatasize.php
+++ core_dev/trunk/tests/test.ConvertDatasize.php
@@ -8,5 +8,8 @@ $d = new ConvertDatasize();
 if ($d->conv('megabyte', 'byte', 0.5) != 524288)  echo "FAIL 1\n";
 if ($d->conv('megabit', 'megabyte', 100) != 12.5) echo "FAIL 2\n";
 if ($d->convLiteral('1GB', 'MiB') != 1024)        echo "FAIL 3\n";
+if ($d->conv('zb', 'tb', 1) != 1073741824)        echo "FAIL 4\n";
+if ($d->conv('zettabyte', 'exabyte', 1) != 1024)  echo "FAIL 5\n";
+if ($d->conv('zettabit', 'exabit', 1) != 1024)    echo "FAIL 6\n";
 
 ?>
