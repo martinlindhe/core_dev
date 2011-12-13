72409147161b67f5319261ade00ed645f249414e
Martin Lindhe <martin@ubique.se>
2011-12-13 12:34:26 +0000

    moved "admin" overview view to its own view

---
+++ core_dev/trunk/views/admin/admin.php
@@ -0,0 +1,23 @@
+<?php
+
+$session->requireSuperAdmin();
+
+echo '<h1>core_dev admin</h1>';
+echo ahref('a/moderation', 'Moderation queue').'<br/>';
+echo '<br/>';
+echo ahref('a/users',          'Users').'<br/>';
+echo ahref('a/usergroups',     'User groups').'<br/>';
+echo ahref('a/files',          'Files').'<br/>';
+echo ahref('a/chatroom/list',  'Chatrooms').'<br/>';
+echo ahref('a/userdata/list',  'Userdata types').'<br/>';
+echo ahref('a/reserved_words', 'Reserved words').'<br/>';
+echo '<br/>';
+echo ahref('a/phpinfo',        'phpinfo()').'<br/>';
+echo ahref('a/compatiblity',   'Compatibility check').'<br/>';
+echo ahref('t/timezones',      'Time zones').'<br/>';
+echo ahref('t/currency',       'Currencies').'<br/>';
+echo '<br/>';
+echo ahref('a/mysql_config',   'MySQL information').'<br/>';
+echo '<br/>';
+
+?>
--- core_dev/trunk/views/core/coredev.php
+++ core_dev/trunk/views/core/coredev.php
@@ -2,7 +2,7 @@
 
 //STATUS: wip
 
-//TODO: ability to attach project-specific admin pages here
+//TODO: separate these to their own files
 
 require_once('UserList.php');
 require_once('UserGroupList.php');
@@ -60,28 +60,6 @@ case 'image':
     $im->render();
 
     return;
-
-case 'admin':
-    $session->requireSuperAdmin();
-
-    echo '<h1>core_dev admin</h1>';
-    echo ahref('a/moderation', 'Moderation queue').'<br/>';
-    echo '<br/>';
-    echo ahref('a/users',          'Users').'<br/>';
-    echo ahref('a/usergroups',     'User groups').'<br/>';
-    echo ahref('a/files',          'Files').'<br/>';
-    echo ahref('a/chatroom/list',  'Chatrooms').'<br/>';
-    echo ahref('a/userdata/list',  'Userdata types').'<br/>';
-    echo ahref('a/reserved_words', 'Reserved words').'<br/>';
-    echo '<br/>';
-    echo ahref('a/phpinfo',        'phpinfo()').'<br/>';
-    echo ahref('a/compatiblity',   'Compatibility check').'<br/>';
-    echo ahref('t/timezones',      'Time zones').'<br/>';
-    echo ahref('t/currency',       'Currencies').'<br/>';
-    echo '<br/>';
-    echo ahref('a/mysql_config',   'MySQL information').'<br/>';
-    echo '<br/>';
-    break;
 
 case 'robots':
     $page->disableDesign(); //remove XhtmlHeader, designHead & designFoot for this request
