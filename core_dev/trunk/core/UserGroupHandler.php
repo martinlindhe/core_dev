16df469a1d6a3891c972823f7af70a03d6100f6c
Martin Lindhe <martin@ubique.se>
2012-01-18 21:12:22 +0000

    sql cosmetics

--- core_dev/trunk/core/UserGroupHandler.php
+++ core_dev/trunk/core/UserGroupHandler.php
@@ -7,10 +7,12 @@
 
 class UserGroupHandler
 {
+    protected static $tbl_name = 'tblGroupMembers';
+
     public static function getUserLevel($id)
     {
         $q =
-        'SELECT t2.level FROM tblGroupMembers AS t1'.
+        'SELECT t2.level FROM '.self::$tbl_name.' AS t1'.
         ' INNER JOIN tblUserGroups AS t2 ON (t1.groupId=t2.groupId)'.
         ' WHERE t1.userId = ?'.
         ' ORDER BY t2.level DESC LIMIT 1';
@@ -22,7 +24,9 @@ class UserGroupHandler
     /** Returns a list of UserGroup objects for all groups the user is a member of */
     public static function getGroups($user_id)
     {
-        $q = 'SELECT groupId FROM tblGroupMembers WHERE userId = ?';
+        $q =
+        'SELECT groupId FROM '.self::$tbl_name.
+        ' WHERE userId = ?';
         $res = Sql::pSelect1d($q, 'i', $user_id);
 
         $groups = array();
@@ -35,18 +39,24 @@ class UserGroupHandler
     /** Adds the user to a user group */
     public static function addToGroup($user_id, $grp_id)
     {
-        $q = 'SELECT COUNT(*) FROM tblGroupMembers WHERE groupId = ? AND userId = ?';
+        $q =
+        'SELECT COUNT(*) FROM '.self::$tbl_name.
+        ' WHERE groupId = ? AND userId = ?';
         if (Sql::pSelectItem($q, 'ii', $grp_id, $user_id))
             return true;
 
-        $q = 'INSERT INTO tblGroupMembers SET groupId = ?, userId = ?';
+        $q =
+        'INSERT INTO '.self::$tbl_name.
+        ' SET groupId = ?, userId = ?';
         Sql::pInsert($q, 'ii', $grp_id, $user_id);
         return true;
     }
 
     public static function removeFromGroup($user_id, $grp_id)
     {
-        $q = 'DELETE FROM tblGroupMembers WHERE groupId = ? AND userId = ?';
+        $q =
+        'DELETE FROM '.self::$tbl_name.
+        ' WHERE groupId = ? AND userId = ?';
         Sql::pDelete($q, 'ii', $grp_id, $user_id);
         return true;
     }
