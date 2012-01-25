d24bf1e3a8d828996a64f797bb7afbe6e6547889
Martin Lindhe <martin@ubique.se>
2012-01-25 00:40:27 +0000

    DROP mediaprobe (moved to other repo) since its unused, has huge sample files & is a C application

--- core_dev/trunk/tools/mediaprobe/probe_bmp.h
+++
@@ -1,6 +0,0 @@
-#ifndef PROBE_BMP_H
-#define PROBE_BMP_H
-
-int probe_bmp(FILE *f, int len, int info);
-
-#endif /* PROBE_BMP_H */
--- core_dev/trunk/tools/mediaprobe/probe_png.h
+++
@@ -1,6 +0,0 @@
-#ifndef PROBE_PNG_H
-#define PROBE_PNG_H
-
-int probe_png(FILE *f, int len, int info);
-
-#endif /* PROBE_PNG_H */
