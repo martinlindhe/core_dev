0a31abab6efe449b560cdcd88fd4dc5ee52b5836
Martin Lindhe <martin@ubique.se>
2012-05-03 15:08:24 +0000

    user/search/usersonline: new view listing all users online

--- core_dev/trunk/views/user/search.php
+++ core_dev/trunk/views/user/search.php
@@ -29,7 +29,25 @@ case 'user':
     $form->setHandler('handleSearch');
 
     echo $form->render();
+
+    echo ahref('u/search/usersonline', 'Show users online');
     break;
+
+case 'usersonline':
+    $list = UserList::getUsersOnline();
+    echo '<h2>Showing all users online';
+
+    echo ' ('.count($list).' hits)</h2>';
+
+    $dt = new YuiDatatable();
+    $dt->addColumn('id',    'Username', 'link', 'u/profile/', 'name');
+    $dt->addColumn('time_last_active',  'Last active');
+    $dt->addColumn('type',              'Type', 'array', getSessionTypes() );
+    $dt->setDataSource( $list );
+    //$dt->setRowsPerPage(10);
+    echo $dt->render();
+    break;
+
 
 default:
     throw new Exception ('no such view: '.$this->owner);
