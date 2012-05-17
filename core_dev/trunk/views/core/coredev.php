eb9f91452574b64432650c463fd0cccaba5f887d
Martin Lindhe <martin@ubique.se>
2012-05-17 02:32:20 +0000

    separate selftest into own view

--- core_dev/trunk/views/core/coredev.php
+++ core_dev/trunk/views/core/coredev.php
@@ -1,29 +1,12 @@
 <?php
 
-//TODO: drop this file, separate each view to their own file
+//TODO: drop this file when all selftest-url checkers are updated
 
 switch ($this->view) {
 case 'selftest':
-    // returns a error code if anything is wrong
-    // the idea is to have a external script fetch http://server/coredev/selftest
-    // and warn if result != "STATUS:OK"
-    $status = 'OK';
-
-    $dir = $page->getUploadPath();
-    if ($dir) {
-        if (!is_dir($dir))
-            $status = 'ERROR: upload dir dont exist';
-        else if (!is_writable($dir))
-            $status = 'ERROR: upload dir not writable';
-
-        // low disk space in upload directories?
-        $df = disk_free_space($dir);
-        if ($df < 1024 * 1024 * 32) // 32mb
-            $status = 'ERROR: not enough space in upload dir';
-    }
-
-    die('STATUS:'.$status);
-
+    $view = new ViewModel(__DIR__.'/selftest.php');
+    echo $view->render();
+    return;
 
 default:
     throw new Exception ('no such view: '.$this->view);
---
+++ core_dev/trunk/views/core/selftest.php
@@ -0,0 +1,24 @@
+<?php
+// returns a error code if anything is wrong
+// the idea is to have a external script fetch http://server/coredev/selftest
+// and warn if result != "STATUS:OK"
+
+$page->disableDesign();
+$status = 'OK';
+
+$dir = $page->getUploadPath();
+if ($dir) {
+    if (!is_dir($dir))
+        $status = 'ERROR: upload dir dont exist';
+    else if (!is_writable($dir))
+        $status = 'ERROR: upload dir not writable';
+
+    // low disk space in upload directories?
+    $df = disk_free_space($dir);
+    if ($df < 1024 * 1024 * 32) // 32mb
+        $status = 'ERROR: not enough space in upload dir';
+}
+
+echo 'STATUS:'.$status;
+
+?>
