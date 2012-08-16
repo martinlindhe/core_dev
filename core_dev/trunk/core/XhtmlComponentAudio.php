efef9a7b6672737b4517d0ead6cf194745fd94a3
Martin Lindhe <martin@ubique.se>
2012-08-16 15:42:56 +0000

    reworl XhtmlComponentAudio to allow for multiple audio tracks to be embedded, like
        $a = new XhtmlComponentAudio();
        $a->preload = 'none';
        $a->addComponent('/recording/listen/'.$s['id'].'/wav', 'audio/wav');
        $a->addComponent('/recording/listen/'.$s['id'].'/mp3', 'audio/mp3');
        echo $a->render();

--- core_dev/trunk/core/XhtmlComponentAudio.php
+++ core_dev/trunk/core/XhtmlComponentAudio.php
@@ -2,7 +2,7 @@
 /**
  * $Id$
  *
- * @author Martin Lindhe, 2011 <martin@startwars.org>
+ * @author Martin Lindhe, 2011-2012 <martin@startwars.org>
  */
 
 //STATUS: wip
@@ -18,19 +18,40 @@
 
 require_once('XhtmlComponent.php');
 
-class XhtmlComponentAudio extends XhtmlComponent
+class AudioComponent
 {
     var $src;
     var $mimetype = '';
+}
+
+class XhtmlComponentAudio extends XhtmlComponent
+{
+    var $src;
     var $preload = 'auto'; ///< "auto", "metadata" or "none"
+    private $components = array();
+
+    function addComponent($src, $mimetype = '')
+    {
+        $c = new AudioComponent();
+        $c->src = $src;
+        $c->mimetype = $mimetype;
+
+        $this->components[] = $c;
+    }
 
     function render()
     {
-        return
-            '<audio controls="controls" preload="'.$this->preload.'">'.
-                '<source src="'.$this->src.'"'.($this->mimetype ? ' type="'.$this->mimetype.'"' : '').'/>'.
-                'Your browser does not support the audio element.'.
-            '</audio>';
+        $res =
+        '<audio controls="controls" preload="'.$this->preload.'">';
+
+        foreach ($this->components as $c)
+            $res .= '<source src="'.$c->src.'"'.($c->mimetype ? ' type="'.$c->mimetype.'"' : '').'/>';
+
+        $res .=
+            'Your browser does not support the audio element.'.
+        '</audio>';
+
+        return $res;
     }
 
 }
