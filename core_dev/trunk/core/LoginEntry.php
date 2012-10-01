23f09e619cc1e0270e94c763acc3e1db075df562
Martin Lindhe <martin@ubique.se>
2012-10-01 13:33:45 +0000

    make LoginEntry able to log different types of login entries

--- core_dev/trunk/core/LoginEntry.php
+++ core_dev/trunk/core/LoginEntry.php
@@ -9,43 +9,45 @@
 
 namespace cd;
 
+define('LOGIN_INTERNAL', 1);
+
 class LoginEntry
 {
 
     protected static $tbl_name = 'tblLogins';
 
-    public static function add($user_id, $ip, $user_agent)
+    public static function add($user_id, $ip, $user_agent, $type = LOGIN_INTERNAL)
     {
         $q =
         'INSERT INTO '.self::$tbl_name.
-        ' SET timeCreated = NOW(), userId = ?, IP = ?, userAgent = ?';
-        return Sql::pInsert($q, 'iss', $user_id, $ip, $user_agent);
+        ' SET timeCreated = NOW(), userId = ?, IP = ?, userAgent = ?, type = ?';
+        return Sql::pInsert($q, 'issi', $user_id, $ip, $user_agent, $type);
     }
 
-    public static function getHistory($user_id)
+    public static function getHistory($user_id, $type = LOGIN_INTERNAL)
     {
         $q =
         'SELECT * FROM '.self::$tbl_name.
-        ' WHERE userId = ? ORDER BY timeCreated DESC';
-        return Sql::pSelect($q, 'i', $user_id);
+        ' WHERE userId = ? AND type = ? ORDER BY timeCreated DESC';
+        return Sql::pSelect($q, 'ii', $user_id, $type);
     }
 
     /** @return array of user id's associated with $ip */
-    public static function getUsersByIP($ip)
+    public static function getUsersByIP($ip, $type = LOGIN_INTERNAL)
     {
         $q =
         'SELECT DISTINCT(userId) FROM '.self::$tbl_name.
-        ' WHERE IP = ?';
-        return Sql::pSelect1d($q, 's', $ip);
+        ' WHERE IP = ? AND type = ?';
+        return Sql::pSelect1d($q, 'si', $ip, $type);
     }
 
     /** @return array of IP's associated with user_id */
-    public static function getIPsByUser($user_id)
+    public static function getIPsByUser($user_id, $type = LOGIN_INTERNAL)
     {
         $q =
         'SELECT DISTINCT(IP) FROM '.self::$tbl_name.
-        ' WHERE userId = ?';
-        return Sql::pSelect1d($q, 'i', $user_id);
+        ' WHERE userId = ? AND type = ?';
+        return Sql::pSelect1d($q, 'ii', $user_id, $type);
     }
 
 }
