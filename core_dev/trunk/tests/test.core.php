09d1965130ea7b19d24921a7468976c35d3a67b7
Martin Lindhe <martin@ubique.se>
2012-10-08 17:24:08 +0000

    add tests for control characters

--- core_dev/trunk/tests/test.core.php
+++ core_dev/trunk/tests/test.core.php
@@ -15,6 +15,8 @@ array("strpad_exact('1234', 8, ' ')",       '1234    '),
 array("strpad_exact('1234', 3, ' ')",       '123'),
 array("is_alphanumeric('2')",               true),
 array("is_alphanumeric('2.0')",             true),
+array("is_alphanumeric('%')",               false),  // important not to allow control characters
+array("is_alphanumeric('/')",               false),  // important not to allow control characters
 array("is_alphanumeric('x\"x')",            false),  //  " is NOT ok
 array("is_alphanumeric(\"x'x\")",           false),  //  ' is NOT ok
 array("is_alphanumeric('abc 123')",         false),  // space is NOT ok
