1d32eb043c413723c9b1bc1b1b05f5641c1f9946
Martin Lindhe <martin@ubique.se>
2012-12-12 00:11:55 +0000

    clean up tests

--- core_dev/trunk/tests/test.html.php
+++ core_dev/trunk/tests/test.html.php
@@ -6,39 +6,38 @@ set_include_path(get_include_path() . PATH_SEPARATOR . dirname(__FILE__).'/../co
 
 require_once('html.php');
 
-if (htmlchars_decode('ja&nbsp;ha') != 'ja ha')  echo "FAIL 1\n"; // space char is a special NBSP character
-if (htmlchars_decode('reg&reg;me') != 'reg®me') echo "FAIL 2\n";
+if (htmlchars_decode('ja&nbsp;ha') != 'ja ha')        echo "FAIL 1\n"; // space char is a special NBSP character
+if (htmlchars_decode('reg&reg;me') != 'reg®me')       echo "FAIL 2\n";
+if (htmlchars_decode('&#039;') != "'")                echo "FAIL 3\n";
 
 
-$html =
+if (is_html_color('#fft0ff'))                         echo "FAIL 10\n";
+if (is_html_color('#ff0ff'))                          echo "FAIL 11\n";
+if (is_html_color('#ffff'))                           echo "FAIL 12\n";
+if (is_html_color('#ff'))                             echo "FAIL 13\n";
+if (is_html_color('#f'))                              echo "FAIL 14\n";
+if (is_html_color('aff'))                             echo "FAIL 15\n";
+if (is_html_color(''))                                echo "FAIL 16\n";
+if (!is_html_color('#fff'))                           echo "FAIL 17\n";
+if (!is_html_color('#FFF'))                           echo "FAIL 18\n";
+if (!is_html_color('#ff00ff'))                        echo "FAIL 19\n";
+if (!is_html_color('#FF00FF'))                        echo "FAIL 20\n";
+
+
+if (relurl('/') != '/')                               echo "FAIL 30\n";
+if (relurl('?val') != '?val')                         echo "FAIL 31\n";
+if (relurl('abp://subscribe') != 'abp://subscribe')   echo "FAIL 32\n";
+
+
+$s =
 'hello '.
 '<style>.box h1 {text-align:left;}</style>'.
 '<style type="text/css">.xxx</style>'.
 '<script>strip me</script>'.
 '<script language="text/javascript">strip me</script>'.
 'world';
+if (strip_html($s) != 'hello world')                  echo "FAIL 40\n";
 
-if (strip_html($html) != 'hello world') echo "FAIL 3\n";
+if (strip_html('hi<!--comment--> bye') != 'hi bye')   echo "FAIL 41\n";
 
-if (relurl('/') != '/')        echo "FAIL 4\n";
-if (relurl('?val') != '?val')  echo "FAIL 5\n";
-
-if (!is_html_color('#fff'))    echo "FAIL 6\n";
-
-if (!is_html_color('#FFF'))    echo "FAIL 7\n";
-if (!is_html_color('#ff00ff')) echo "FAIL 8\n";
-if (!is_html_color('#FF00FF')) echo "FAIL 9\n";
-
-if (is_html_color('#fft0ff'))  echo "FAIL 10\n";
-
-if (is_html_color('#ff0ff'))   echo "FAIL 11\n";
-if (is_html_color('#ffff'))    echo "FAIL 12\n";
-if (is_html_color('#ff'))      echo "FAIL 13\n";
-if (is_html_color('#f'))       echo "FAIL 14\n";
-if (is_html_color('aff'))      echo "FAIL 15\n";
-if (is_html_color(''))         echo "FAIL 16\n";
-
-if (htmlchars_decode('&#039;') != "'") echo "FAIL 17\n";
-
-if (relurl('abp://subscribe') != 'abp://subscribe') echo "FAIL 20\n";
 ?>
