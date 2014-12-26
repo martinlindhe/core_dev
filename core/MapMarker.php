3cf5bfef077cd8f1ae16164e171a81e024a71f16
Martin Lindhe <martin@ubique.se>
2014-12-26 17:42:31 +0100

    move MapMarker class to own file, in order to work with autoloading

---
+++ core/MapMarker.php
@@ -0,0 +1,20 @@
+<?php
+echo "WOWO";die;
+namespace cd;
+
+class MapMarker
+{
+    var $latitude;
+    var $longitude;
+    var $tooltip;
+    var $icon;
+    var $zIndex;
+    var $flat = false;
+
+    function __construct($lat = 0, $lng = 0)
+    {
+        $this->latitude  = $lat;
+        $this->longitude = $lng;
+    }
+}
+
--- core/MapWidget.php
+++ core/MapWidget.php
@@ -9,23 +9,6 @@
 
 namespace cd;
 
-class MapMarker
-{
-    var $latitude;
-    var $longitude;
-    var $tooltip;
-    var $icon;
-    var $zIndex;
-    var $flat = false;
-
-    function __construct($lat = 0, $lng = 0)
-    {
-        $this->latitude  = $lat;
-        $this->longitude = $lng;
-    }
-
-}
-
 abstract class MapWidget
 {
     protected $width  = 500;
@@ -61,4 +44,3 @@ abstract class MapWidget
     abstract function render();
 }
 
-?>
